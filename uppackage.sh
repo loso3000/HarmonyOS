ca782e4b3da838307d6f1e1eda15a6b203c9bd59
loso3000 <51484662+loso3000@users.noreply.github.com>
2021-09-08 12:34:37 +0800

    ss

--- uppackage.sh
+++ uppackage.sh
@@ -4,7 +4,7 @@
 cp -f ./package/build/banner ./package/base-files/files/etc/
 # date1='${version} Ipv4P-S'`TZ=UTC-8 date +%Y.%m.%d -d +"8"hour`
 date1='Ipv4P-S'`TZ=UTC-8 date +%Y.%m.%d -d +"8"hour`
-sed -i 's/$(IMG_PREFIX_VERCODE)$(IMG_PREFIX_EXTRA)/$(shell TZ=UTC-8 date +%Y%m%d -d +8hour)-Ipv4P/g' include/image.mk
+sed -i 's/$(VERSION_DIST_SANITIZED)-$(IMG_PREFIX_VERNUM)$(IMG_PREFIX_VERCODE)$(IMG_PREFIX_EXTRA)/$(shell TZ=UTC-8 date +%Y%m%d -d +8hour)-Ipv4P/g' include/image.mk
 echo "DISTRIB_REVISION='${date1} by Sirpdboy'" > ./package/base-files/files/etc/openwrt_release1
 echo ${date1}' by Sirpdboy ' >> ./package/base-files/files/etc/banner
 echo '---------------------------------' >> ./package/base-files/files/etc/banner
