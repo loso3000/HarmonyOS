08d329edf0a8e530609a7c3bd7f27c48a85f4565
loso3000 <51484662+loso3000@users.noreply.github.com>
2021-10-31 02:31:26 +0800

    by

--- initipv6.sh
+++ initipv6.sh
@@ -153,20 +153,6 @@ git clone -b master --single-branch https://github.com/tty228/luci-app-servercha
 
 # svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash ./package/diy/luci-app-openclash
 svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/luci-app-openclash
-# rm -rf package/luci-app-openclash/root/usr/share/openclash/yacd
-# wget https://github.com/haishanh/yacd/archive/gh-pages.zip
-# unzip gh-pages.zip
-# mv yacd-gh-pages package/luci-app-openclash/root/usr/share/openclash/yacd
-# rm -rf gh-pages.zip
-# mkdir -p files/etc/openclash/core
-# open_clash_main_url=$(curl -sL https://api.github.com/repos/vernesong/OpenClash/releases/tags/Clash | grep /clash-linux-$1 | sed 's/.*url\": \"//g' | sed 's/\"//g')
-# offical_clash_main_url=$(curl -sL https://api.github.com/repos/Dreamacro/clash/releases/tags/v1.3.5 | grep /clash-linux-$1 | sed 's/.*url\": \"//g' | sed 's/\"//g')
-# clash_tun_url=$(curl -sL https://api.github.com/repos/vernesong/OpenClash/releases/tags/TUN-Premium | grep /clash-linux-$1 | sed 's/.*url\": \"//g' | sed 's/\"//g')
-# clash_game_url=$(curl -sL https://api.github.com/repos/vernesong/OpenClash/releases/tags/TUN | grep /clash-linux-$1 | sed 's/.*url\": \"//g' | sed 's/\"//g')
-# wget -qO- $open_clash_main_url | tar xOvz > files/etc/openclash/core/clash
-# wget -qO- $clash_tun_url | gunzip -c > files/etc/openclash/core/clash_tun
-# wget -qO- $clash_game_url | tar xOvz > files/etc/openclash/core/clash_game
-# chmod +x files/etc/openclash/core/clash*
 
 git clone https://github.com/kiddin9/luci-app-dnsfilter package/lean/luci-app-dnsfilter
 # svn co https://github.com/small-5/luci-app-adblock-plus/trunk/ ./package/diy/luci-app-adblock-plus
