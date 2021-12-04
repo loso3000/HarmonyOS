2f23be41ce2f8c5557e2817c52a7f6e2b8ec6299
loso3000 <51484662+loso3000@users.noreply.github.com>
2021-12-04 17:57:21 +0800

    python

--- script/dz3.sh
+++ script/dz3.sh
@@ -141,6 +141,8 @@ git clone -b master --single-branch https://github.com/tty228/luci-app-servercha
 
 git clone https://github.com/kiddin9/luci-app-dnsfilter package/luci-app-dnsfilter
 
+svn co https://github.com/fqrouter/fqrouter/trunk/package/  ./package/python/
+
 echo '替换aria2'
 rm -rf feeds/luci/applications/luci-app-aria2 && \
 svn co https://github.com/sirpdboy/sirpdboy-package/trunk/luci-app-aria2 feeds/luci/applications/luci-app-aria2
