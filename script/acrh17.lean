d8bcc3a0abd94960934d23e53969172c60b5826c
loso3000 <51484662+loso3000@users.noreply.github.com>
2022-01-15 20:58:28 +0800

    acrh17

---
+++ script/acrh17.lean
@@ -0,0 +1,234 @@
+CONFIG_MODULES=y
+CONFIG_HAVE_DOT_CONFIG=y
+CONFIG_TARGET_ipq40xx=y
+CONFIG_TARGET_ipq40xx_generic=y
+CONFIG_TARGET_ipq40xx_generic=y
+
+CONFIG_HAS_SUBTARGETS=y
+CONFIG_HAS_DEVICES=y
+CONFIG_TARGET_BOARD="ipq40xx"
+CONFIG_TARGET_SUBTARGET="generic"
+CONFIG_TARGET_PROFILE="DEVICE_asus_rt-acrh17"
+CONFIG_TARGET_ARCH_PACKAGES="arm_cortex-a7_neon-vfpv4"
+CONFIG_DEFAULT_TARGET_OPTIMIZATION="-Os -pipe"
+CONFIG_CPU_TYPE="cortex-a7+neon-vfpv4"
+CONFIG_LINUX_4_19=y
+CONFIG_DEFAULT_ath10k-firmware-qca4019-ct=y
+CONFIG_DEFAULT_ath10k-firmware-qca9984-ct=y
+CONFIG_DEFAULT_base-files=y
+CONFIG_DEFAULT_block-mount=y
+CONFIG_DEFAULT_busybox=y
+CONFIG_DEFAULT_ca-certificates=y
+CONFIG_DEFAULT_dnsmasq-full=y
+CONFIG_DEFAULT_dropbear=y
+CONFIG_DEFAULT_ethtool=y
+CONFIG_DEFAULT_firewall=y
+CONFIG_DEFAULT_fstools=y
+CONFIG_DEFAULT_ipq-wifi-asus_rt-acrh17=y
+CONFIG_DEFAULT_iptables=y
+CONFIG_DEFAULT_kmod-ath10k-ct=y
+CONFIG_DEFAULT_kmod-gpio-button-hotplug=y
+CONFIG_DEFAULT_kmod-ipt-raw=y
+CONFIG_DEFAULT_kmod-leds-gpio=y
+CONFIG_DEFAULT_kmod-nf-nathelper=y
+CONFIG_DEFAULT_kmod-nf-nathelper-extra=y
+CONFIG_DEFAULT_kmod-usb-dwc3=y
+CONFIG_DEFAULT_kmod-usb-dwc3-qcom=y
+CONFIG_DEFAULT_kmod-usb-ledtrig-usbport=y
+CONFIG_DEFAULT_kmod-usb3=y
+CONFIG_DEFAULT_libc=y
+CONFIG_DEFAULT_libgcc=y
+CONFIG_DEFAULT_libustream-openssl=y
+CONFIG_DEFAULT_logd=y
+CONFIG_DEFAULT_mtd=y
+CONFIG_DEFAULT_netifd=y
+CONFIG_DEFAULT_opkg=y
+CONFIG_DEFAULT_ppp=y
+CONFIG_DEFAULT_ppp-mod-pppoe=y
+CONFIG_DEFAULT_swconfig=y
+CONFIG_DEFAULT_uci=y
+CONFIG_DEFAULT_uclient-fetch=y
+CONFIG_DEFAULT_urandom-seed=y
+CONFIG_DEFAULT_urngd=y
+CONFIG_DEFAULT_wget=y
+CONFIG_DEFAULT_wpad-openssl=y
+CONFIG_HAS_TESTING_KERNEL=y
+CONFIG_HAS_FPU=y
+CONFIG_AUDIO_SUPPORT=y
+CONFIG_GPIO_SUPPORT=y
+CONFIG_PCI_SUPPORT=y
+CONFIG_PCIE_SUPPORT=y
+CONFIG_USB_SUPPORT=y
+CONFIG_RTC_SUPPORT=y
+CONFIG_USES_DEVICETREE=y
+CONFIG_USES_INITRAMFS=y
+CONFIG_USES_SQUASHFS=y
+CONFIG_NAND_SUPPORT=y
+CONFIG_arm=y
+CONFIG_arm_v7=y
+CONFIG_ARCH="arm"
+
+#
+# Root filesystem images
+#
+# CONFIG_TARGET_ROOTFS_EXT4FS is not set
+CONFIG_TARGET_ROOTFS_SQUASHFS=y
+CONFIG_TARGET_SQUASHFS_BLOCK_SIZE=256
+CONFIG_TARGET_UBIFS_FREE_SPACE_FIXUP=y
+CONFIG_TARGET_UBIFS_JOURNAL_SIZE=""
+
+# 不压缩efi
+# CONFIG_TARGET_IMAGES_GZIP is not set
+# CONFIG_EFI_IMAGES=y
+# CONFIG_TARGET_ROOTFS_TARGZ=n
+# CONFIG_VMDK_IMAGES is not set
+# ipv6
+CONFIG_PACKAGE_ipv6helper=y
+CONFIG_PACKAGE_dnsmasq_full_dhcpv6=y
+# file system
+CONFIG_PACKAGE_kmod-fs-antfs=y
+# CONFIG_PACKAGE_kmod-fs-ntfs  is not set
+#ksmbd
+#CONFIG_PACKAGE_kmod-fs-ksmbd=n
+CONFIG_PACKAGE_kmod-nls-utf8=y
+#CONFIG_PACKAGE_kmod-nls-cp936=y
+#CONFIG_PACKAGE_kmod-nls-iso8859-1=y
+# add upnp
+CONFIG_PACKAGE_miniupnpd-igdv1=y
+CONFIG_PACKAGE_luci-app-upnp=y
+CONFIG_PACKAGE_luci-app-boostupnp=n
+# CONFIG_PACKAGE_luci-app-wol is not set
+CONFIG_PACKAGE_luci-app-wolplus=y
+# 工具
+
+CONFIG_DEFAULT_coremark=y
+CONFIG_DEFAULT_autocore-arm=y
+CONFIG_DEFAULT_automount=y
+CONFIG_DEFAULT_ddns-scripts_aliyun=y
+CONFIG_DEFAULT_ddns-scripts_dnspod=y
+CONFIG_DEFAULT_default-settings=y
+# CONFIG_PACKAGE_autosamba is not set
+CONFIG_PACKAGE_autosamba-ksmbd=n
+CONFIG_PACKAGE_autosamba-samba4=y
+# CONFIG_PACKAGE_luci-app-accesscontrol is not set
+# CONFIG_PACKAGE_luci-app-adbyby-plus is not set
+CONFIG_PACKAGE_luci-app-adguardhome=n
+CONFIG_PACKAGE_luci-app-advanced=n
+CONFIG_PACKAGE_luci-app-autotimeset=n
+CONFIG_PACKAGE_luci-app-rebootschedule=n
+# CONFIG_PACKAGE_luci-app-autoreboot is not set
+CONFIG_PACKAGE_luci-app-control-timewol=n
+CONFIG_PACKAGE_luci-app-control-weburl=n
+CONFIG_PACKAGE_luci-app-control-webrestriction=n
+CONFIG_PACKAGE_luci-app-control-speedlimit=n
+CONFIG_PACKAGE_luci-app-webadmin=n
+CONFIG_PACKAGE_luci-app-cpulimit=y
+CONFIG_PACKAGE_luci-app-diskman=n
+CONFIG_PACKAGE_luci-app-eqos=n
+CONFIG_PACKAGE_luci-app-hd-idle=n
+# CONFIG_PACKAGE_luci-app-ipsec-vpnd is not set
+CONFIG_PACKAGE_luci-app-jd-dailybonus=n
+CONFIG_PACKAGE_luci-app-koolproxyR=n
+CONFIG_PACKAGE_luci-app-netdata=n
+CONFIG_PACKAGE_luci-app-onliner=n
+CONFIG_PACKAGE_luci-app-openclash=n
+CONFIG_PACKAGE_luci-app-passwall=n
+CONFIG_PACKAGE_luci-app-samba=n
+CONFIG_PACKAGE_luci-app-samba4=y
+CONFIG_PACKAGE_luci-app-serverchan=n
+# CONFIG_PACKAGE_luci-app-sfe is no set
+# CONFIG_PACKAGE_luci-app-flowoffload is no set
+# CONFIG_PACKAGE_luci-app-filetransfer is no set
+CONFIG_PACKAGE_luci-app-smartdns=n
+# CONFIG_PACKAGE_luci-app-ssr-plus is no set
+CONFIG_PACKAGE_luci-app-timecontrol=y
+CONFIG_PACKAGE_luci-app-access-control=n
+CONFIG_PACKAGE_luci-app-ttyd=n
+CONFIG_PACKAGE_luci-app-turboacc=y
+CONFIG_PACKAGE_luci-app-turboacc_INCLUDE_shortcut-fe=y
+CONFIG_PACKAGE_luci-app-vssr=n
+CONFIG_PACKAGE_luci-app-wrtbwmon=n
+# CONFIG_PACKAGE_luci-app-nlbwmon is no set
+CONFIG_PACKAGE_luci-app-netspeedtest=n
+CONFIG_PACKAGE_luci-app-dnsto=n
+CONFIG_PACKAGE_luci-app-bypass=y
+# CONFIG_PACKAGE_luci-app-bypass_INCLUDE_NaiveProxy=n
+# CONFIG_PACKAGE_luci-app-bypass_INCLUDE_V2ray=y
+CONFIG_PACKAGE_luci-app-dnsfilter=n
+# CONFIG_PACKAGE_luci-app-vsftpd is no set
+CONFIG_PACKAGE_luci-app-switch-lan-play=n
+CONFIG_PACKAGE_luci-app-mentohust=n
+CONFIG_PACKAGE_luci-app-adblock-plus=n
+# 主题
+CONFIG_PACKAGE_luci-theme-atmaterial_new=n
+CONFIG_PACKAGE_luci-theme-argon=n
+CONFIG_PACKAGE_luci-theme-argon_new=n
+CONFIG_PACKAGE_luci-theme-netgear=n
+CONFIG_PACKAGE_luci-theme-opentomato=n
+CONFIG_PACKAGE_luci-theme-opentomcat=n
+CONFIG_PACKAGE_luci-theme-opentopd=y
+#增加其它插件
+CONFIG_PACKAGE_luci-app-tencentddns=n
+CONFIG_PACKAGE_luci-app-pushbot=n
+CONFIG_PACKAGE_luci-app-easymesh=n
+CONFIG_PACKAGE_luci-app-ksmbd=n
+CONFIG_PACKAGE_luci-app-cifsd=n
+CONFIG_PACKAGE_luci-app-cifs-mount=n
+# CONFIG_PACKAGE_luci-app-xlnetacc is not set
+CONFIG_PACKAGE_luci-app-zerotier=y
+CONFIG_PACKAGE_luci-app-mwan3=n
+# CONFIG_PACKAGE_luci-app-unblockneteasemusic is not set
+# CONFIG_PACKAGE_luci-app-unblockmusic is not set
+CONFIG_UnblockNeteaseMusic_Go=y
+CONFIG_UnblockNeteaseMusic_NodeJS=y
+# CONFIG_PACKAGE_luci-app-minidlna is not set
+# CONFIG_PACKAGE_luci-app-rclone is not set
+# CONFIG_PACKAGE_luci-app-rclone_INCLUDE_fuse-utils is not set
+# CONFIG_PACKAGE_luci-app-rclone_INCLUDE_rclone-ng is not set
+# CONFIG_PACKAGE_luci-app-rclone_INCLUDE_rclone-webui is not set
+CONFIG_PACKAGE_luci-app-pptp-server=n
+CONFIG_PACKAGE_luci-app-pppoe-server=n
+CONFIG_PACKAGE_luci-app-ipsec-server=n
+CONFIG_PACKAGE_luci-app-docker=n
+CONFIG_PACKAGE_luci-app-dockerman=n
+CONFIG_PACKAGE_luci-app-koolddns=n
+CONFIG_PACKAGE_luci-app-syncdial=n
+CONFIG_PACKAGE_luci-app-softethervpn=n
+CONFIG_PACKAGE_luci-app-uugamebooster=n
+CONFIG_DEFAULT_luci-app-cpufreq=n
+CONFIG_PACKAGE_luci-app-udpxy=n
+CONFIG_PACKAGE_luci-app-socat=n
+CONFIG_PACKAGE_luci-app-oaf=n
+CONFIG_PACKAGE_luci-app-transmission=n
+CONFIG_PACKAGE_luci-app-usb-printer=n
+CONFIG_PACKAGE_luci-app-mwan3helper=n
+CONFIG_PACKAGE_luci-app-qbittorrent=n
+CONFIG_PACKAGE_luci-app-familycloud=n
+CONFIG_PACKAGE_luci-app-nps=n
+CONFIG_PACKAGE_luci-app-frpc=n
+CONFIG_PACKAGE_luci-app-nfs=n
+CONFIG_PACKAGE_luci-app-openvpn-server=n
+CONFIG_PACKAGE_luci-app-aria2=n
+CONFIG_PACKAGE_luci-app-openvpn=n
+CONFIG_PACKAGE_luci-app-ttnode=n
+
+CONFIG_PACKAGE_kmod-usb-serial=y
+CONFIG_PACKAGE_kmod-usb-serial-option=y
+CONFIG_PACKAGE_kmod-usb-serial-wwan=y
+CONFIG_PACKAGE_usb-modeswitch=y
+CONFIG_PACKAGE_kmod-mii=y
+CONFIG_PACKAGE_luci-proto-qmi=y
+CONFIG_PACKAGE_qmi-utils=y
+CONFIG_PACKAGE_umbim=y
+CONFIG_PACKAGE_uqmi=y
+CONFIG_PACKAGE_comgt-ncm=y
+CONFIG_PACKAGE_luci-proto-ncm=y
+CONFIG_PACKAGE_comgt=y
+CONFIG_PACKAGE_kmod-usb-acm=y
+CONFIG_PACKAGE_luci-proto-3g=y
+
+CONFIG_PACKAGE_libimobiledevice-utils=y
+CONFIG_PACKAGE_libplist-utils=y
+CONFIG_PACKAGE_libusbmuxd-utils=y
+CONFIG_PACKAGE_usbmuxd=y
+CONFIG_PACKAGE_libudev-fbsd=y
