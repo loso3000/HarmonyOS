d397e0d686361ef88f7d131372296c995f052a9b
loso3000 <51484662+loso3000@users.noreply.github.com>
2022-03-04 18:13:20 +0800

    argon_jitu

--- script/dz3.sh
+++ script/dz3.sh
@@ -87,7 +87,7 @@ rm -rf ./feeds/luci/applications/luci-app-samba4
 # curl -fsSL https://raw.githubusercontent.com/loso3000/other/master/patch/autocore/files/arm/index.htm > package/lean/autocore/files/arm/index.htm
 curl -fsSL  https://raw.githubusercontent.com/loso3000/other/master/patch/default-settings/zzz-default-settings2 > ./package/build/default-settings/files/zzz-default-settings
 sed -i 's/argon/argon-jitu/g'    ./package/build/default-settings/files/zzz-default-settings
-#sed -i 's/opentopd/argon-jitu/g'    ./package/build/default-settings/files/zzz-default-settings
+#sed -i 's/opentopd/argon_jitu/g'    ./package/build/default-settings/files/zzz-default-settings
 sed -i '/DISTRIB_DESCRIPTION=/d'   ./package/build/default-settings/files/zzz-default-settings
 # curl -fsSL  https://raw.githubusercontent.com/sirpdboy/sirpdboy-package/master/set/sysctl.conf > ./package/base-files/files/etc/sysctl.conf
 echo '添加关机'
@@ -263,7 +263,7 @@ sed -i 's/KERNEL_PATCHVER:=5.15/KERNEL_PATCHVER:=5.10/g' ./target/linux/*/Makefi
 # sed -i "s/bootstrap/chuqitopd/g" feeds/luci/modules/luci-base/root/etc/config/luci
 # sed -i 's/bootstrap/chuqitopd/g' feeds/luci/collections/luci/Makefile
 echo "修改默认主题"
-sed -i 's/+luci-theme-bootstrap/+luci-theme-argon-jitu/g' feeds/luci/collections/luci/Makefile
+sed -i 's/+luci-theme-bootstrap/+luci-theme-argon_jitu/g' feeds/luci/collections/luci/Makefile
 sed -i "s/bootstrap/argon-jitu/g" feeds/luci/modules/luci-base/root/etc/config/luci
 sed -i '/set luci.main.mediaurlbase=\/luci-static\/argon-jitu/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
 
