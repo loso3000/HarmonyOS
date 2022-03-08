cf58e5e680349512706e344fc788ca21c5b3cd89
loso3000 <51484662+loso3000@users.noreply.github.com>
2022-03-08 17:09:45 +0800

    pass

--- dz.sh
+++ dz.sh
@@ -476,8 +476,8 @@ git clone -b master --single-branch https://github.com/destan19/OpenAppFilter ./
 svn co https://github.com/xiaorouji/openwrt-passwall/trunk package/pass2/
 #rm -rf package/passwall/shadowsocksr-libev
 # svn co https://github.com/loso3000/openwrt-pswall/trunk/luci-app-passwall ./package/passwall/luci-app-passwall
-
 svn co https://github.com/loso3000/openwrt-pswall/trunk/ ./package/passwall/
+
 # svn co https://github.com/loso3000/openwrt-pswall/trunk/shadowsocksr-libev ./package/passwall/shadowsocksr-libev
 # svn co https://github.com/loso3000/openwrt-pswall/trunk/v2ray ./package/passwall/v2ray
 # svn co https://github.com/loso3000/openwrt-pswall/trunk/xray ./package/passwall/xray
