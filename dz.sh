d17da8a116787ab05ead8b9bd8e958fd0d7f55f4
loso3000 <51484662+loso3000@users.noreply.github.com>
2022-03-13 11:28:52 +0800

    dd

--- dz.sh
+++ dz.sh
@@ -64,10 +64,11 @@ rm -rf ./package/lean/trojan
 # rm -rf ./feeds/luci/applications/luci-app-vlmcsd
 # rm -rf ./feeds/luci/applications/vlmcsd 
 
+rm -rf ./packages/build/ddns-scripts_dnspod
 rm -rf ./package/lean/ddns-scripts_aliyun
-rm -rf ./package/lean/ddns-scripts_dnspod
+# rm -rf ./package/lean/ddns-scripts_dnspod
 svn co https://github.com/sirpdboy/build/trunk/ddns-scripts_aliyun package/lean/ddns-scripts_aliyun
-svn co https://github.com/sirpdboy/build/trunk/ddns-scripts_dnspod package/lean/ddns-scripts_dnspod
+# svn co https://github.com/sirpdboy/build/trunk/ddns-scripts_dnspod package/lean/ddns-scripts_dnspod
 
 # rm -rf ./feeds/luci/applications/luci-app-zerotier
 # rm -rf ./feeds/packages/net/zerotier
