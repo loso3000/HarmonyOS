17d2381b2a92de6cf1dff1f398a134a5f0e19445
loso3000 <51484662+loso3000@users.noreply.github.com>
2022-03-14 09:55:45 +0800

    sam

--- dz.sh
+++ dz.sh
@@ -82,7 +82,7 @@ rm -rf ./package/network/services/samba36
 svn co https://github.com/sirpdboy/build/trunk/samba36 ./package/network/services/samba36
 
 # rm -rf ./package/build/samba4
-rm -rf package/feeds/packages/samba4
+# rm -rf package/feeds/packages/samba4
 # rm -rf ./feeds/package/net/samba4 && svn co https://github.com/sirpdboy/build/trunk/samba4 ./feeds/package/net/samba4
 rm -rf ./feeds/luci/applications/luci-app-samba4
 
