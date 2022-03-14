4fcb86fc693ffc91cc9cf49a4f4d5328fbbae764
loso3000 <51484662+loso3000@users.noreply.github.com>
2022-03-14 09:54:02 +0800

    sab

--- script/dz3.sh
+++ script/dz3.sh
@@ -335,7 +335,7 @@ rm -rf ./feeds/luci/applications/luci-app-samba
 svn co https://github.com/sirpdboy/build/trunk/luci-app-samba ./feeds/luci/applications/luci-app-samba
 rm -rf ./package/network/services/samba36 
 svn co https://github.com/sirpdboy/build/trunk/samba36 ./package/network/services/samba36
-rm -rf package/feeds/packages/samba4
+# rm -rf package/feeds/packages/samba4
 # rm -rf ./feeds/package/net/samba4 && svn co https://github.com/sirpdboy/build/trunk/samba4 ./feeds/package/net/samba4
 rm -rf ./feeds/luci/applications/luci-app-samba4
 # Boost 通用即插即用
