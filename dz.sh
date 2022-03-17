29274f84d2509318d3671ce863e286570c2fa593
loso3000 <51484662+loso3000@users.noreply.github.com>
2022-03-17 14:46:48 +0800

    de sa

--- dz.sh
+++ dz.sh
@@ -82,7 +82,7 @@ svn co https://github.com/sirpdboy/build/trunk/luci-app-samba ./feeds/luci/appli
 rm -rf ./package/network/services/samba36 
 svn co https://github.com/sirpdboy/build/trunk/samba36 ./package/network/services/samba36
 
-# rm -rf ./package/build/samba4
+rm -rf ./package/build/samba4
 rm -rf ./feeds/packages/net/samba4 && svn co https://github.com/sirpdboy/sirpdboy-package/trunk/samba4 ./feeds/packages/net/samba4
 rm -rf ./feeds/luci/applications/luci-app-samba4 && svn co https://github.com/sirpdboy/build/trunk/luci-app-samba4 ./feeds/luci/applications/luci-app-samba4
 
