4d9d6e6f0ed6b72ce1283dcdd0485c0f9bf2c53d
loso3000 <51484662+loso3000@users.noreply.github.com>
2022-04-14 11:18:32 +0800

    xx

--- script/jz.sh
+++ script/jz.sh
@@ -57,8 +57,9 @@ svn co https://github.com/messense/aliyundrive-webdav/trunk/openwrt/aliyundrive-
 svn co https://github.com/messense/aliyundrive-webdav/trunk/openwrt/luci-app-aliyundrive-webdav ./package/lean/luci-app-aliyundrive-webdav
 
 # Add ddnsto & linkease
-svn co https://github.com/linkease/nas-packages-luci/trunk/luci/ ./package/lean/luci-app-linkease
-svn co https://github.com/linkease/nas-packages/trunk/network/services/./package/lean//linkease
+svn co https://github.com/linkease/nas-packages-luci/trunk/luci/ ./package/lean/luci
+svn co https://github.com/linkease/nas-packages/trunk/network/services/ ./package/lean/linkease
+svn co https://github.com/linkease/istore/trunk/luci/ ./package/lean/istore
 sed -i 's/1/0/g' ./package/lean/linkease/linkease/files/linkease.config
 
 #zerotier 
