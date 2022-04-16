309203d749c39012b5f7875124f66f011bad9af5
loso3000 <51484662+loso3000@users.noreply.github.com>
2022-04-17 02:18:40 +0800

    zz

--- script/dz3.sh
+++ script/dz3.sh
@@ -201,8 +201,9 @@ git clone -b master --single-branch https://github.com/destan19/OpenAppFilter ./
 # svn export https://github.com/openwrt/packages/trunk/utils/runc/Makefile ./feeds/packages/utils/runc/Makefile
 
 # Add luci-app-dockerman
+
 rm -rf ./feeds/luci/collections/luci-lib-docker
-git clone --depth=1 https://github.com/lisaac/luci-lib-docker ./feeds/luci/collections/luci-lib-docker
+git clone --depth=1 https://github.com/lisaac/luci-lib-docker ./feeds/luci/collections/lib-docker
 
 rm -rf ./packages/build/ddns-scripts_dnspod
 rm -rf ./package/lean/ddns-scripts_aliyun && \
