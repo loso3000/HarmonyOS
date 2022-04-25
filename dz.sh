41a2b9fef8e6833dbdf0a5d8430de3db1e123ea5
loso3000 <51484662+loso3000@users.noreply.github.com>
2022-04-25 15:59:59 +0800

    dd

--- dz.sh
+++ dz.sh
@@ -21,8 +21,8 @@ rm -rf ./feeds/luci/themes/luci-theme-argon
 rm -rf ./feeds/luci/applications/luci-app-wrtbwmon
 rm -rf ./feeds/luci/applications/luci-app-https-dns-proxy
 rm -rf ./package/other/up/oaf
-rm -rf ./feeds/packages/net/https-dns-proxy
-svn export https://github.com/Lienol/openwrt-packages/trunk/net/https-dns-proxy feeds/packages/net/https-dns-proxy
+# rm -rf ./feeds/packages/net/https-dns-proxy
+# svn export https://github.com/Lienol/openwrt-packages/trunk/net/https-dns-proxy feeds/packages/net/https-dns-proxy
 
 git clone -b master --single-branch https://github.com/destan19/OpenAppFilter ./package/diy/OpenAppFilter
 
