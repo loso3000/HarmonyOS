ccb496b8b40ead15cce3659423e60087905694e3
loso3000 <51484662+loso3000@users.noreply.github.com>
2022-04-28 15:21:22 +0800

    dd

--- script/initipv6.sh
+++ script/initipv6.sh
@@ -232,9 +232,9 @@ git clone --depth=1 https://github.com/tindy2013/openwrt-subconverter  package/n
 # svn export https://github.com/openwrt/packages/trunk/utils/runc/Makefile ./feeds/packages/utils/runc/Makefile
 
 # Add luci-app-dockerman
-rm -rf ../../customfeeds/luci/collections/luci-lib-docker
-rm -rf ../../customfeeds/luci/applications/luci-app-docker
-rm -rf ../../customfeeds/luci/applications/luci-app-dockerman
+rm -rf feeds/luci/collections/luci-lib-docker
+rm -rf feeds/luci/applications/luci-app-docker
+rm -rf feeds/luci/applications/luci-app-dockerman
 # git clone --depth=1 https://github.com/lisaac/luci-app-dockerman ./feeds/luci/collections/luci-lib-docker
 git clone --depth=1 https://github.com/lisaac/luci-lib-docker  ./feeds/luci/collections/luci-lib-docker
 git clone --depth=1 https://github.com/lisaac/luci-lib-docker  packages/luci/luci-lib-docker
