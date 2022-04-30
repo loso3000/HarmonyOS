172a813cfb3d2872bf9afa414db80913bec4dae8
loso3000 <51484662+loso3000@users.noreply.github.com>
2022-04-30 16:35:33 +0800

    xx

--- script/initipv6.sh
+++ script/initipv6.sh
@@ -33,6 +33,17 @@ svn export https://github.com/immortalwrt/packages/branches/openwrt-18.06/utils/
 
 rm -rf ./feeds/packages/utils/containerd
 svn export https://github.com/immortalwrt/packages/branches/openwrt-18.06/utils/containerd ./feeds/packages/utils/containerd
+
+# Add luci-app-dockerman
+rm -rf package/build/luci-app-dockerman
+
+#rm -rf feeds/luci/applications/luci-app-docker
+#rm -rf feeds/luci/applications/luci-app-dockerman
+# git clone --depth=1 https://github.com/lisaac/luci-app-dockerman ./feeds/luci/collections/luci-lib-docker
+# git clone --depth=1 https://github.com/lisaac/luci-lib-docker  ./feeds/luci/collections/luci-lib-docker
+# git clone --depth=1 https://github.com/lisaac/luci-lib-docker  packages/luci/luci-lib-docker
+
+
 # CPU 控制相关
 rm -rf ./feeds/luci/applications/luci-app-cpufreq 
 svn export https://github.com/immortalwrt/luci/trunk/applications/luci-app-cpufreq feeds/luci/applications/luci-app-cpufreq
@@ -230,14 +241,6 @@ git clone --depth=1 https://github.com/tindy2013/openwrt-subconverter  package/n
 
 # rm -rf ./feeds/packages/utils/runc/Makefile
 # svn export https://github.com/openwrt/packages/trunk/utils/runc/Makefile ./feeds/packages/utils/runc/Makefile
-
-# Add luci-app-dockerman
-# rm -rf feeds/luci/collections/luci-lib-docker
-rm -rf feeds/luci/applications/luci-app-docker
-rm -rf feeds/luci/applications/luci-app-dockerman
-# git clone --depth=1 https://github.com/lisaac/luci-app-dockerman ./feeds/luci/collections/luci-lib-docker
-# git clone --depth=1 https://github.com/lisaac/luci-lib-docker  ./feeds/luci/collections/luci-lib-docker
-# git clone --depth=1 https://github.com/lisaac/luci-lib-docker  packages/luci/luci-lib-docker
 
 rm -rf ./packages/build/ddns-scripts_dnspod
 rm -rf ./package/lean/ddns-scripts_aliyun
