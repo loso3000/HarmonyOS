330323bcd213c7fa353bf806ea184e3ae1f3568a
loso3000 <51484662+loso3000@users.noreply.github.com>
2022-05-14 22:28:35 +0800

    xx

--- script/initipv6.sh
+++ script/initipv6.sh
@@ -48,11 +48,14 @@ echo '替换smartdns'
 rm -rf ./feeds/packages/net/smartdns
 svn co https://github.com/sirpdboy/sirpdboy-package/trunk/smartdns ./feeds/packages/net/smartdns
 
-rm -rf ./package/diy/luci-app-netdata 
-rm -rf ./package/diy/netdata
+# rm -rf ./package/diy/luci-app-netdata 
+# rm -rf ./package/diy/netdata
 rm -rf ./feeds/luci/applications/luci-app-netdata 
 svn co https://github.com/sirpdboy/sirpdboy-package/trunk/luci-app-netdata ./feeds/luci/applications/luci-app-netdata
 rm -rf ./feeds/packages/admin/netdata && svn co https://github.com/sirpdboy/sirpdboy-package/trunk/netdata ./feeds/packages/admin/netdata
+
+
+rm -rf ./feeds/packages/net/adguardhome && svn co https://github.com/sirpdboy/sirpdboy-package/trunk/adguardhome ./feeds/packages/net/adguardhome
 
 cat  ./package/build/mwan3/files/etc/config/mwan3   > ./feeds/packages/net/mwan3/files/etc/config/mwan3 && rm -rf ./package/build/mwan3
 # sed -i 's,+mwan3,+mwan3plus,g' ./feeds/luci/applications/luci-app-mwan3/Makefile
