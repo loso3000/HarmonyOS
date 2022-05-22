41ece0cad2f541c6b226a3dcba61ab6df8fe6f49
loso3000 <51484662+loso3000@users.noreply.github.com>
2022-05-22 09:54:03 +0800

    net

--- dz.sh
+++ dz.sh
@@ -50,10 +50,14 @@ svn co https://github.com/sirpdboy/sirpdboy-package/trunk/smartdns ./feeds/packa
 
 # netdata 
 # rm -rf ./package/diy/luci-app-netdata 
+rm -rf ./package/diy/luci-app-netdata/root
 rm -rf ./package/diy/netdata
-# rm -rf ./feeds/luci/applications/luci-app-netdata 
+cp -rf ./feeds/packages/admin/netdata    ./package/other/netdata    
+rm -rf ./feeds/luci/applications/luci-app-netdata
+rm -rf ./feeds/packages/admin/netdata
 # svn co https://github.com/sirpdboy/sirpdboy-package/trunk/luci-app-netdata ./feeds/luci/applications/luci-app-netdata
 # rm -rf ./feeds/packages/admin/netdata && svn co https://github.com/sirpdboy/sirpdboy-package/trunk/netdata ./feeds/packages/admin/netdata
+
 
 #mwan3
 cat  ./package/build/mwan3/files/etc/config/mwan3   > ./feeds/packages/net/mwan3/files/etc/config/mwan3 && rm -rf ./package/build/mwan3
