a32f3010d604bce9afd8fedd85d895a8874d39f8
loso3000 <51484662+loso3000@users.noreply.github.com>
2022-05-23 17:37:19 +0800

    net

--- script/initmini.sh
+++ script/initmini.sh
@@ -44,11 +44,11 @@ rm -rf ./package/build/mwan3
 # rm -rf ./feeds/packages/net/mwan3 && svn co https://github.com/sirpdboy/build/trunk/mwan3 ./feeds/packages/net/mwan3
 
 # netdata 
-# rm -rf ./package/diy/luci-app-netdata 
-# rm -rf ./package/diy/netdata
-rm -rf ./feeds/luci/applications/luci-app-netdata 
+cp -rf ./feeds/packages/admin/netdata ./package/diy/
+rm -rf ./feeds/luci/applications/luci-app-netdata
+rm -rf ./feeds/packages/admin/netdata
 svn co https://github.com/sirpdboy/sirpdboy-package/trunk/luci-app-netdata ./feeds/luci/applications/luci-app-netdata
-rm -rf ./feeds/packages/admin/netdata && svn co https://github.com/sirpdboy/sirpdboy-package/trunk/netdata ./feeds/packages/admin/netdata
+# rm -rf ./feeds/packages/admin/netdata && svn co https://github.com/sirpdboy/sirpdboy-package/trunk/netdata ./feeds/packages/admin/netdata
 
 # rm -rf ./feeds/packages/net/https-dns-proxy  && svn co https://github.com/Lienol/openwrt-packages/trunk/net/https-dns-proxy ./feeds/packages/net/https-dns-proxy
 # rm -rf ./feeds/packages/devel/ninja   && svn co https://github.com/Lienol/openwrt-packages/trunk/devel/ninja feeds/packages/devel/ninja
