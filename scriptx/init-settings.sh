c82553f92c763ecfaadb6a13d745f48561144b5b
loso3000 <51484662+loso3000@users.noreply.github.com>
2022-05-27 23:43:11 +0800

    aa

--- scriptx/init-settings.sh
+++ scriptx/init-settings.sh
@@ -4,10 +4,15 @@
 uci set luci.main.mediaurlbase='/luci-static/opentopd'
 
 # Disable IPV6 ula prefix
-# sed -i 's/^[^#].*option ula/#&/' /etc/config/network
+sed -i 's/^[^#].*option ula/#&/' /etc/config/network
 
 # Check file system during boot
 uci set fstab.@global[0].check_fs=1
 uci commit
+
+if [ ! -f "/boot/init.sh" ]; then
+bash /boot/init.sh
+rm -f /boot/init.sh || true
+fi
 
 exit 0
