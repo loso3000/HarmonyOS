0a62513150a8a9b27c32178018d57b68f752e566
loso3000 <51484662+loso3000@users.noreply.github.com>
2022-05-27 19:24:26 +0800

    xx

--- dz.sh
+++ dz.sh
@@ -223,6 +223,26 @@ svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash ./package
 
 # Add subconverter
 # git clone --depth=1 https://github.com/tindy2013/openwrt-subconverter  package/new/
+# 订阅转换
+svn export https://github.com/immortalwrt/packages/trunk/net/subconverter feeds/packages/net/subconverter
+#wget https://github.com/immortalwrt/packages/raw/b7b4499/net/subconverter/Makefile -O feeds/packages/net/subconverter/Makefile
+#mkdir -p ./feeds/packages/net/subconverter/patches
+#wget https://github.com/immortalwrt/packages/raw/b7b4499/net/subconverter/patches/100-stdcxxfs.patch -O feeds/packages/net/subconverter/patches/100-stdcxxfs.patch
+#sed -i '\/bin\/subconverter/a\\t$(STAGING_DIR_HOST)/bin/upx --lzma --best $(1)/usr/bin/subconverter' feeds/packages/net/subconverter/Makefile
+ln -sf ../../../feeds/packages/net/subconverter ./package/feeds/packages/subconverter
+svn export https://github.com/immortalwrt/packages/trunk/libs/jpcre2 feeds/packages/libs/jpcre2
+ln -sf ../../../feeds/packages/libs/jpcre2 ./package/feeds/packages/jpcre2
+svn export https://github.com/immortalwrt/packages/trunk/libs/rapidjson feeds/packages/libs/rapidjson
+ln -sf ../../../feeds/packages/libs/rapidjson ./package/feeds/packages/rapidjson
+svn export https://github.com/immortalwrt/packages/trunk/libs/libcron feeds/packages/libs/libcron
+#wget https://github.com/immortalwrt/packages/raw/b7b4499/libs/libcron/Makefile -O feeds/packages/libs/libcron/Makefile
+ln -sf ../../../feeds/packages/libs/libcron ./package/feeds/packages/libcron
+svn export https://github.com/immortalwrt/packages/trunk/libs/quickjspp feeds/packages/libs/quickjspp
+#wget https://github.com/immortalwrt/packages/raw/b7b4499/libs/quickjspp/Makefile -O feeds/packages/libs/quickjspp/Makefile
+ln -sf ../../../feeds/packages/libs/quickjspp ./package/feeds/packages/quickjspp
+svn export https://github.com/immortalwrt/packages/trunk/libs/toml11 feeds/packages/libs/toml11
+ln -sf ../../../feeds/packages/libs/toml11 ./package/feeds/packages/toml11
+
 
 # rm -rf ./feeds/packages/utils/runc/Makefile
 # svn export https://github.com/openwrt/packages/trunk/utils/runc/Makefile ./feeds/packages/utils/runc/Makefile
