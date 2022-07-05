1bea3c19b0de2aa964659f8d630c1c9d1ec710ca
loso3000 <51484662+loso3000@users.noreply.github.com>
2022-07-05 23:37:49 +0800

    xx

--- scriptx/init-settings.sh
+++ scriptx/init-settings.sh
@@ -1,15 +1,32 @@
 #!/bin/bash
 
+local ifname ifname2 lannet wannet a b i
+
 # Set default theme to luci-theme-opentopd
 uci set luci.main.mediaurlbase='/luci-static/opentopd'
-
-# Disable IPV6 ula prefix
-sed -i 's/^[^#].*option ula/#&/' /etc/config/network
-
 # Check file system during boot
 uci set fstab.@global[0].check_fs=1
-uci commit
-
+	
+if ! ifname=$(uci get network.wan.ifname 2>/dev/null) ; then
+      	 ifname=$(uci get network.lan.ifname 2>/dev/null) 
+fi
+	ifname2=$(echo $ifname | sed -r 's/([a-z]{1,})([0-9]{1,}).*/\1\ \2/'  | awk -F ' '  '{print $1}')
+	a=$(ip address | grep ^[0-9] | awk -F: '{print $2}' | sed "s/ //g" | grep $ifname2 | grep -v "@" | grep -v "\." )
+	b=$(echo "$a" | wc -l)
+	lannet=""
+	for i in $(seq 1 $b)
+	do
+		if [ $i = $b -a  $b != 1 ]; then
+			wannet=$(echo "$a" | sed -n ${i}p)
+		else
+			lannet="$lannet $(echo "$a" | sed -n ${i}p)"
+		fi
+	done
+	uci -q set network.lan.ifname="${lannet}"
+  [ ${lannet} != ${wannet} ] && uci -q set network.wan.ifname="${wannet}"
+uci commit luci
+uci commit network
+uci commit fstab
 if [ ! -f "/boot/init.sh" ]; then
 bash /boot/init.sh
 rm -f /boot/init.sh || true
