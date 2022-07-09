cf253dfa0f6b0c9798cf62342bd55b4030615875
loso3000 <51484662+loso3000@users.noreply.github.com>
2022-07-09 14:13:44 +0800

    ip

--- scriptx/init-settings.sh
+++ scriptx/init-settings.sh
@@ -3,8 +3,8 @@
 uci set luci.main.mediaurlbase='/luci-static/opentopd'
 # Check file system during boot
 uci set fstab.@global[0].check_fs=1
-if ! ifname=$(uci get network.wan.ifname 2>/dev/null) ; then
-      	 ifname=$(uci get network.lan.ifname 2>/dev/null) 
+if ! ifname=$(uci -q get network.wan.ifname 2>/dev/null) ; then
+      	 ifname=$(uci -q get network.lan.ifname 2>/dev/null) 
 fi
 ifname2=$(echo $ifname | sed -r 's/([a-z]{1,})([0-9]{1,}).*/\1\ \2/'  | awk -F ' '  '{print $1}')
 a=$(ip address | grep ^[0-9] | awk -F: '{print $2}' | sed "s/ //g" | grep $ifname2 | grep -v "@" | grep -v "\." )
@@ -13,19 +13,14 @@ if [ ${b} -gt 1 ]; then
 	  lannet=""
 	  for i in $(seq 1 $b)
 	  do
-		if [ $i = $b ]; then
-			wannet=$(echo "$a" | sed -n ${b}p)
+		if [ $(uci -q get network.wan.ifname) = $(echo "$a" | sed -n ${b}p) ]; then
+			wannet=$(uci -q get network.wan.ifname)
 		else
 			lannet="$lannet $(echo "$a" | sed -n ${i}p)"
 		fi
 	  done
-      uci -q delete network.wan
-      uci -q set network.wan=interface
       uci -q set network.wan.ifname="$wannet"
       uci -q set network.wan6.ifname="$wannet"
-      uci -q set network.wan.proto='dhcp'
-      uci -q set network.wan.metric='40'
-      uci -q set network.wan.delegate='0'
       uci -q set network.lan.ifname="${lannet}"
 fi
 uci commit luci
