a17c696e19058d459d904efc7b1402bd6027777e
loso3000 <51484662+loso3000@users.noreply.github.com>
2022-07-20 03:03:47 +0800

    ds

--- r2s.lean
+++ r2s.lean
@@ -229,7 +229,7 @@ CONFIG_PACKAGE_luci-app-openvpn=n
 
 CONFIG_PACKAGE_luci-app-beardropper=y
 CONFIG_PACKAGE_luci-app-control-parentcontrol=y
-# CONFIG_PACKAGE_luci-app-wizard=y
+# CONFIG_PACKAGE_luci-app-wizard=n
 # CONFIG_PACKAGE_luci-app-filebrowser=y
 # CONFIG_PACKAGE_luci-app-fileassistant=y
 
