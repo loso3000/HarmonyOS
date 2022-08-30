bb5802ce843236d486f141d62b8bf738f2403c61
loso3000 <51484662+loso3000@users.noreply.github.com>
2022-08-30 12:15:06 +0800

    as

--- script/jz.sh
+++ script/jz.sh
@@ -27,8 +27,11 @@ rm -rf ./package/other/up/https-dns-proxy
 
 # git clone -b master --single-branch https://github.com/destan19/OpenAppFilter ./package/diy/OpenAppFilter
 
+
 git clone https://github.com/sbwml/openwrt-alist.git package/openwrt-alist
 sed -i 's/网络存储/存储/g' ./package/openwrt-alist/luci-app-alist/po/zh-cn/alist.po
+sed -i 's/nas/services/g' ./package/openwrt-alist/luci-app-alist/luasrc/controller/alist.lua   #alistalist
+sed -i 's/nas/services/g' ./package/openwrt-alist/luci-app-alist/luasrc/view/alist_status.htm   #alist
 
 echo '替换smartdns'
 rm -rf ./feeds/packages/net/smartdns
