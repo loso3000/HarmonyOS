185374a6d1ad23e7757537270c3a5cd51fffbfcd
loso3000 <51484662+loso3000@users.noreply.github.com>
2022-09-16 00:22:27 +0800

    a

--- script/initipv6.sh
+++ script/initipv6.sh
@@ -53,6 +53,7 @@ cat  ./package/build/mwan3/files/etc/config/mwan3   > ./feeds/packages/net/mwan3
 
 rm -rf ./package/build/autocore
 rm -rf ./package/build/default-settings
+rm -rf ./package/lean/automount  && svn co https://github.com/sirpdboy/build/trunk/automount ./package/lean/automount
 rm -rf ./package/lean/autocore  && svn co https://github.com/sirpdboy/build/trunk/autocore ./package/lean/autocore
 rm -rf ./package/lean/default-settings  && svn co https://github.com/sirpdboy/build/trunk/default-settings ./package/lean/default-settings
 
