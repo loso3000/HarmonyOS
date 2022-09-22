d8beb8574a8203c9f5461b19b13399a589d077ba
loso3000 <51484662+loso3000@users.noreply.github.com>
2022-09-22 20:23:43 +0800

    xx

--- imminip.sh
+++ imminip.sh
@@ -400,7 +400,7 @@ elif [ "$VER2" = "5.15" ]; then
     sed -i 's/$(VERSION_DIST_SANITIZED)-$(IMG_PREFIX_VERNUM)$(IMG_PREFIX_VERCODE)$(IMG_PREFIX_EXTRA)/$(shell TZ=UTC-8 date +%Y%m%d -d +12hour)-MINI-BYPASS-5.15-/g' include/image.mk
 fi
 
-echo "DISTRIB_REVISION="${date1} ' by Sirpdboy ' > ./package/base-files/files/etc/openwrt_release1
+echo "DISTRIB_REVISION='${date1} 'by Sirpdboy '" > ./package/base-files/files/etc/openwrt_release1
 echo ${date1}' by Sirpdboy ' >> ./package/base-files/files/etc/banner
 echo '---------------------------------' >> ./package/base-files/files/etc/banner
 
