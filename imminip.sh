2037c962cf43b2175029eae9fa2116a5412232a2
loso3000 <51484662+loso3000@users.noreply.github.com>
2022-09-26 21:55:38 +0800

    10

--- imminip.sh
+++ imminip.sh
@@ -381,9 +381,9 @@ sed -i "s/192.168.6.1/192.168.8.1/g"  package/base-files/files/bin/config_genera
 ver1=`grep "KERNEL_PATCHVER:="  target/linux/x86/Makefile | cut -d = -f 2` #判断当前默认内核版本号如5.10
 export VER2="$(grep "KERNEL_PATCHVER:="  ./target/linux/x86/Makefile | cut -d = -f 2)"
 
-date1='Bypass-Mini-R'`TZ=UTC-8 date +%Y.%m.%d -d +"12"hour`
-#date1='Ipv6-R20220824 by Sirpdboy '
-sed -i 's/$(VERSION_DIST_SANITIZED)-$(IMG_PREFIX_VERNUM)$(IMG_PREFIX_VERCODE)$(IMG_PREFIX_EXTRA)/$(shell TZ=UTC-8 date +%Y%m%d -d +12hour)-Bypass-Mini-/g' include/image.mk
+date1='Ipv4-Bypass-R'`TZ=UTC-8 date +%Y.%m.%d -d +"12"hour`
+date1='Ipv4-Bypass-R2022.10.01 by Sirpdboy '
+sed -i 's/$(VERSION_DIST_SANITIZED)-$(IMG_PREFIX_VERNUM)$(IMG_PREFIX_VERCODE)$(IMG_PREFIX_EXTRA)/2022101-Ipv4-Bypass-5.15-/g' include/image.mk
 if [ "$VER2" = "5.4" ]; then
     sed -i 's/$(VERSION_DIST_SANITIZED)-$(IMG_PREFIX_VERNUM)$(IMG_PREFIX_VERCODE)$(IMG_PREFIX_EXTRA)/$(shell TZ=UTC-8 date +%Y%m%d -d +12hour)-Bypass-Mini-5.4-/g' include/image.mk
 elif [ "$VER2" = "5.10" ]; then
