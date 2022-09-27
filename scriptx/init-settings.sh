4ab44e42e6b0bc9d4762048f8a7d02797d6dead2
loso3000 <51484662+loso3000@users.noreply.github.com>
2022-09-27 23:04:21 +0800

    qq

--- scriptx/init-settings.sh
+++ scriptx/init-settings.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 # Set default theme to luci-theme-opentopd
 # uci set luci.main.mediaurlbase='/luci-static/opentopd'
-uci set fstab.@global[0].check_fs=1
-	ifname=$(uci get network.lan.ifname 2>/dev/null) 
+uci -q set fstab.@global[0].check_fs=1
+	ifname=$(uci -q get network.lan.ifname 2>/dev/null) 
 	ifname2=$(echo $ifname | sed -r 's/([a-z]{1,})([0-9]{1,}).*/\1\ \2/'  | awk -F ' '  '{print $1}')#!/bin/bash
 
 local ifname ifname2 lannet wannet a b i
 
-if ! ifname=$(uci get network.wan.ifname 2>/dev/null) ; then
-      	 ifname=$(uci get network.lan.ifname 2>/dev/null) 
+if ! ifname=$(uci -q get network.wan.ifname 2>/dev/null) ; then
+      	 ifname=$(uci -q get network.lan.ifname 2>/dev/null) 
 fi
 ifname2=$(echo $ifname | sed -r 's/([a-z]{1,})([0-9]{1,}).*/\1\ \2/'  | awk -F ' '  '{print $1}')
 a=$(ip address | grep ^[0-9] | awk -F: '{print $2}' | sed "s/ //g" | grep $ifname2 | grep -v "@" | grep -v "\." | awk -F '@' {'print $1'} )
@@ -17,7 +17,7 @@ if [ ${b} -gt 1 ]; then
 	  lannet=""
 	  for i in $(seq 1 $b)
 	  do
-		if [ $i = $b ]; then
+		if [ $i = $ifname ]; then
 			wannet=$(echo "$a" | sed -n ${b}p)
 		else
 			lannet="$lannet $(echo "$a" | sed -n ${i}p)"
@@ -44,7 +44,7 @@ exit 0
 	  lannet=""
 	  for i in $(seq 1 $b)
 	  do
-		[ "$(uci get network.wan.ifname)" = "$(echo "$a" | sed -n ${i}p)" ] || lannet="${lannet} $(echo "$a" | sed -n ${i}p)"
+		[ "$(uci -q get network.wan.ifname)" = "$(echo "$a" | sed -n ${i}p)" ] || lannet="${lannet} $(echo "$a" | sed -n ${i}p)"
 	  done
 	  uci -q set network.lan.ifname="${lannet}"
 	}
