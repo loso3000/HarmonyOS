e9e9ba3dcb7da2f66f8a276d042a1504389f9578
loso3000 <51484662+loso3000@users.noreply.github.com>
2022-09-29 00:19:41 +0800

    c

--- script/ipv6all.sh
+++ script/ipv6all.sh
@@ -14,6 +14,14 @@ git clone https://github.com/loso3000/other ./package/other
 rm -rf ./feeds/luci/themes/luci-theme-argon
 rm -rf ./feeds/luci/applications/luci-app-wrtbwmon
 
+# rm -rf ./package/cupsd/luci-app-cupsd
+rm -rf ./feeds/packages/utils/cupsd
+rm -rf ./feeds/packages/utils/cups-bjnp
+rm -rf ./feeds/luci/applications/luci-app-cupsd
+# svn export https://github.com/immortalwrt/packages/branches/openwrt-18.06/utils/cupsd ./package/build/cupsd
+# svn export https://github.com/immortalwrt/packages/branches/openwrt-18.06/utils/cups-bjnp./package/build/cups-bjnp 
+# svn export https://github.com/immortalwrt/luci/branches/openwrt-18.06-k5.4/applications/luci-app-cupsd ./package/cupsd/luci-app-cupsd
+
 rm -rf ./feeds/packages/net/mosdns
 
 rm -rf ./package/other/up/oaf
