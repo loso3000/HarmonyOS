7945090aa05b7490e8890091a0d06ae5fe128a0e
loso3000 <51484662+loso3000@users.noreply.github.com>
2022-10-12 10:35:23 +0800

    set

--- scriptx/init-settings.sh
+++ scriptx/init-settings.sh
@@ -4,7 +4,6 @@ uci -q set fstab.@global[0].check_fs=1
 ifname=$(uci -q get network.wan.ifname 2>/dev/null)
 [ ! $ifname ] && ifname=$(uci -q get network.lan.ifname 2>/dev/null)
 ifname2=$(echo $ifname | sed -r 's/([a-z]{1,})([0-9]{1,}).*/\1\ \2/'  | awk -F ' '  '{print $1}')
-ifname2=$(echo $ifname | sed -r 's/([a-z]{1,})([0-9]{1,}).*/\1\ \2/'  | awk -F ' '  '{print $1}')
 lannet=""
 for i in $(ip address | awk -F ': ' '/eth[0-9]+/ {print $2}' | awk -F '@' {'print $1'} | xargs)
 do
