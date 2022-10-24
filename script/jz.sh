41bef301881945f2429352e25fb88ad47ae075b0
loso3000 <51484662+loso3000@users.noreply.github.com>
2022-10-24 09:41:48 +0800

    zz

--- script/jz.sh
+++ script/jz.sh
@@ -54,7 +54,7 @@ cat  ./package/build/mwan3/files/etc/config/mwan3   > ./feeds/packages/net/mwan3
 # rm -rf ./package/lean/autosamba
 # rm -rf ./feeds/luci/applications/luci-app-accesscontrol
 
-rm -rf .feeds/packages/utils/coremark/
+# rm -rf .feeds/packages/utils/coremark/
 #upnp
 rm -rf ./feeds/packages/net/miniupnp
 rm -rf ./feeds/luci/applications/luci-app-upnp
