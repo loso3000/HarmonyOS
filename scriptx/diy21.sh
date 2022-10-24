5f21e74dcf4bdc2f6026814598d72e0f618ae6d2
loso3000 <51484662+loso3000@users.noreply.github.com>
2022-10-24 09:51:47 +0800

    zz

--- scriptx/diy21.sh
+++ scriptx/diy21.sh
@@ -52,9 +52,12 @@ sed -i 's/luci-lib-ipkg/luci-base/g' package/diy1/istore/luci-app-store/Makefile
 
 rm -rf ./feeds/packages/net/mosdns
 #upnp
-# rm -rf ./feeds/packages/net/miniupnp
-# rm -rf ./feeds/luci/applications/luci-app-upnp
-rm -rf  ./package/diy/upnpd
+rm -rf ./feeds/packages/net/miniupnp
+rm -rf ./feeds/luci/applications/luci-app-upnp
+# rm -rf  ./package/diy/upnpd
+
+rm -rf .feeds/packages/utils/coremark
+
 
 rm -rf ./package/lean/autocore  
 rm -rf  package/emortal/autocore && svn co https://github.com/sirpdboy/build/trunk/autocore ./package/lean/autocore
