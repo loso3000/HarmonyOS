bb1828a2b0c1f5d54a7785fcc5d41afc542f1aea
loso3000 <51484662+loso3000@users.noreply.github.com>
2022-10-26 12:57:21 +0800

    a

--- script/aipv6mini.sh
+++ script/aipv6mini.sh
@@ -60,8 +60,11 @@ rm -rf ./package/other/up/https-dns-proxy
 
 # git clone -b master --single-branch https://github.com/destan19/OpenAppFilter ./package/diy/OpenAppFilter
 
+
 git clone https://github.com/sbwml/openwrt-alist.git package/openwrt-alist
 sed -i 's/网络存储/存储/g' ./package/openwrt-alist/luci-app-alist/po/zh-cn/alist.po
+rm -rf feeds/packages/lang/golang
+svn export https://github.com/sbwml/packages_lang_golang/branches/19.x feeds/packages/lang/golang
 
 echo '替换smartdns'
 rm -rf ./feeds/packages/net/smartdns
