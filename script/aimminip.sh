3ace989004c8137a5e5c7b09f016074bb6c3bb56
loso3000 <51484662+loso3000@users.noreply.github.com>
2022-10-28 21:59:46 +0800

    aa

--- script/aimminip.sh
+++ script/aimminip.sh
@@ -383,7 +383,7 @@ export VER2="$(grep "KERNEL_PATCHVER:="  ./target/linux/x86/Makefile | cut -d = 
 
 date1='Ipv4-Bypass-Vip-R'`TZ=UTC-8 date +%Y.%m.%d -d +"12"hour`
 # date1='Ipv4-Bypass-Vip-R2022.11.01'
-sed -i 's/$(VERSION_DIST_SANITIZED)-$(IMG_PREFIX_VERNUM)$(IMG_PREFIX_VERCODE)$(IMG_PREFIX_EXTRA)/20221101-Ipv4-Bypass-Vip-5.10-/g' include/image.mk
+# sed -i 's/$(VERSION_DIST_SANITIZED)-$(IMG_PREFIX_VERNUM)$(IMG_PREFIX_VERCODE)$(IMG_PREFIX_EXTRA)/20221101-Ipv4-Bypass-Vip-5.10-/g' include/image.mk
 if [ "$VER2" = "5.4" ]; then
     sed -i 's/$(VERSION_DIST_SANITIZED)-$(IMG_PREFIX_VERNUM)$(IMG_PREFIX_VERCODE)$(IMG_PREFIX_EXTRA)/$(shell TZ=UTC-8 date +%Y%m%d -d +12hour)-Ipv4-Bypass-Vip-5.4-/g' include/image.mk
 elif [ "$VER2" = "5.10" ]; then
