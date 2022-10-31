c2434f21c7e49c312206f745cb3eacfec6a7c950
loso3000 <51484662+loso3000@users.noreply.github.com>
2022-11-01 03:32:14 +0800

    xx

--- script/bjz.sh
+++ script/bjz.sh
@@ -449,7 +449,7 @@ curl -fsSL  https://raw.githubusercontent.com/loso3000/other/master/patch/defaul
 ver1=`grep "KERNEL_PATCHVER:="  target/linux/x86/Makefile | cut -d = -f 2` #判断当前默认内核版本号如5.10
 export VER2="$(grep "KERNEL_PATCHVER:="  ./target/linux/x86/Makefile | cut -d = -f 2)"
 
-date1='Ipv6-Super-R'`TZ=UTC-8 date +%Y.%m.%d -d +"12"hour`' by Sirpdboy '
+date1='Ipv6-Super-Sirpdboy-R'`TZ=UTC-8 date +%Y.%m.%d -d +"12"hour`
 date1='Ipv6-Super-R2022.11.01'
 sed -i 's/$(VERSION_DIST_SANITIZED)-$(IMG_PREFIX_VERNUM)$(IMG_PREFIX_VERCODE)$(IMG_PREFIX_EXTRA)/20221101-Ipv6-Super-5.15-/g' include/image.mk
 if [ "$VER2" = "5.4" ]; then
