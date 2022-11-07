815f2ac01c94a092f5607166a6aa2d73624709eb
loso3000 <51484662+loso3000@users.noreply.github.com>
2022-11-07 10:03:53 +0800

    sbs

--- script/aipv6all.sh
+++ script/aipv6all.sh
@@ -218,7 +218,7 @@ rm -rf ./feeds/luci/applications/luci-app-unblockneteasemusic
 git clone https://github.com/immortalwrt/luci-app-unblockneteasemusic.git  ./package/diy/luci-app-unblockneteasemusic
 sed -i 's/解除网易云音乐播放限制/解锁灰色歌曲/g' ./package/diy/luci-app-unblockneteasemusic/luasrc/controller/unblockneteasemusic.lua
 
-#断线不重拨
+# 断线不重拨
 sed -i 's/q reload/q restart/g' ./package/network/config/firewall/files/firewall.hotplug
 
 #echo "其他修改"
