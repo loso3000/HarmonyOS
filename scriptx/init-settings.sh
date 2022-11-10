3916b60b09a53b334d9d68f9f80152a47b8f5b08
loso3000 <51484662+loso3000@users.noreply.github.com>
2022-11-10 11:23:49 +0800

    ss

--- scriptx/init-settings.sh
+++ scriptx/init-settings.sh
@@ -1,46 +1,6 @@
 #!/bin/bash
 # Set default theme to luci-theme-opentopd
 # uci set luci.main.mediaurlbase='/luci-static/opentopd'
-fdiskB(){
-        a=$1
-		b=$2
-
-		isP=`fdisk -l /dev/$a |grep -v "bytes"|grep "/dev/$a$b"`
-		if [ "$isP" = "" ];then
-				#Start partition
-				fdisk /dev/$1 << EOF
-n
-p
-
-
-
-wq
-EOF
-			sleep 5
-		fi
-		isR=`df -P|grep "/mnt/$a$b"`
-		if [ "$isR" != "" ] ; then
-			umount /mnt/$a$b
-			sleep 5
-		fi
-		isP=`fdisk -l /dev/$a |grep -v "bytes"|grep "/dev/$a$b"`
-		isPa=$(fdisk -l  |grep /dev/${a}2 | awk -F ' ' '{print $4}')
-		isPb=$(fdisk -l  |grep /dev/$a$b | awk -F ' ' '{print $4}')
-		check=`echo "$isPa < $isPb" | bc`
-		if [ "$isP" != "" -a $check = 1 ];then
-			    echo y | mkfs.ext4 /dev/$a$b
-			    block detect > /etc/config/fstab
-			    eval $(block info "/dev/$a$b" | grep -o -e "UUID=\S*")
-			    uci set fstab.@mount[0].uuid="${UUID}"
-			    uci set fstab.@mount[0].target='/overlay'             
-			    uci set fstab.@mount[0].enabled='0'
-			    sed -i "s,/mnt/$a$b,/overlay,g"  /etc/config/fstab
-			    uci commit fstab
-				echo $isPb > /etc/fdiskb.list
-				sleep 5
-				reboot
-		fi
-}
 
 uci -q set fstab.@global[0].check_fs=1
 if ! ifname=$(uci -q get network.wan.ifname 2>/dev/null) ; then
@@ -60,36 +20,5 @@ b=$(echo "$a" | wc -l)
 	}
 uci commit network
 uci commit fstab
-
-[ -f /etc/fdiskb.list ] && exit 0
-
-for i in `cat /proc/partitions|grep -v name|grep -v ram|awk '{print $4}'|grep -v '^$'|grep -v '[0-9]$'|grep -v 'vda'|grep -v 'xvda'|grep -e 'vd' -e 'sd' -e 'xvd'`
-	do
-	
-		isB=`df -P|grep '/boot'  | head -n1 | awk -F ' ' '{print $1}'`
-		case "$i" in
-		sda*)
-			isD=`fdisk -l /dev/sda |grep -v 'bytes'| grep '/dev/sda1'`
-			if [ "$isD" != "" -a "$isB" = "/dev/sda1" ]; then 
-				fdiskB sda 3
-			fi
-			;;
-		nvme0n1*)
-		
-			isD=`fdisk -l /dev/nvme0n1 |grep -v 'bytes'| grep '/dev/nvme0n1p1'`
-			if [ "$isD" != "" -a "$isB" = "/dev/nvme0n1p1" ]; then 
-				fdiskB nvme0n1 p3
-			fi
-			;;
-		mmcblk0*)
-			isD=`fdisk -l /dev/mmcblk0 |grep -v 'bytes'| grep '/dev/mmcblk0p1'`
-			if [ "$isD" != "" -a "$isB" = "/dev/mmcblk0p1" ]; then 
-				fdiskB mmcblk0 p3
-			fi
-			;;
-		esac
-	done
-
-
 	
 exit 0
