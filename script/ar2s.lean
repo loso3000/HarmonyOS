eea7fbf5ecfebb8f2f2b2167aa790a9c0a203cf0
loso3000 <51484662+loso3000@users.noreply.github.com>
2022-11-13 14:21:20 +0800

    xx

--- script/ar2s.lean
+++ script/ar2s.lean
@@ -247,6 +247,7 @@ CONFIG_PACKAGE_luci-app-aria2=n
 CONFIG_PACKAGE_luci-app-openvpn=n
 CONFIG_PACKAGE_luci-app-ddns-go=y
 CONFIG_PACKAGE_luci-app-lucky=y
+CONFIG_PACKAGE_luci-app-partexp=y
 # store
 CONFIG_PACKAGE_kmod-fuse=y
 
