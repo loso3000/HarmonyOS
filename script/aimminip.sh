2f9aaeb0b52322c73317b222e7296426aa70977d
loso3000 <51484662+loso3000@users.noreply.github.com>
2022-11-29 16:14:36 +0800

    xx

--- script/aimminip.sh
+++ script/aimminip.sh
@@ -200,9 +200,11 @@ rm -rf ./feeds/luci/applications/aliyundrive-webdav
 svn export https://github.com/messense/aliyundrive-webdav/trunk/openwrt/aliyundrive-webdav ./feeds/luci/applications/aliyundrive-webdav
 svn export https://github.com/messense/aliyundrive-webdav/trunk/openwrt/luci-app-aliyundrive-webdav ./feeds/luci/applications/luci-app-aliyundrive-webdav 
 # samba4
+rm -rf ./package/other/up/samba4 ./feeds/packages/net/samba4  package/feeds/packages/samba4
+# mv ./package/other/up/samba4 ./feeds/packages/net/samba4 
+svn export https://github.com/loso3000/other/trunk/up/samba4 ./feeds/packages/net/samba4
 rm -rf ./feeds/luci/applications/luci-app-samba4 &&svn export https://github.com/loso3000/other/trunk/up/luci-app-samba4 ./feeds/luci/applications/luci-app-samba4
-rm -rf ./feeds/packages/net/samba4 &&svn export https://github.com/loso3000/other/trunk/up/samba4 ./feeds/packages/net/samba4
-rm -rf ./package/other/up/samba4
+
 #zerotier 
 # rm -rf  luci-app-zerotier && git clone https://github.com/rufengsuixing/luci-app-zerotier.git feeds/luci/applications/luci-app-zerotier  #取消防火墙
 # svn export https://github.com/immortalwrt/luci/trunk/applications/luci-app-zerotier feeds/luci/applications/luci-app-zerotier
@@ -273,9 +275,9 @@ rm -rf ./feeds/luci/applications/luci-app-openclash package/feeds/packages/luci-
 svn export https://github.com/vernesong/OpenClash/branches/dev/luci-app-openclash ./feeds/luci/applications/luci-app-openclash
 
 #bypass
-#rm -rf package/other/up/pass/luci-app-bypass package/feeds/packages/luci-app-bypass
+rm -rf package/other/up/pass/luci-app-bypass package/feeds/packages/luci-app-bypass
 rm -rf ./feeds/luci/applications/luci-app-ssr-plus  package/feeds/packages/luci-app-ssr-plus
-#git clone https://github.com/kiddin9/openwrt-bypass package/bypass
+git clone https://github.com/kiddin9/openwrt-bypass package/bypass
 sed -i 's,default n,default y,g' package/other/up/pass/luci-app-bypass/Makefile
 sed -i 's,default n,default y,g' package/other/up/pass/luci-app-ssr-plus/Makefile
 sed -i 's,default n,default y,g' package/other/up/pass/luci-app-ssr-plusdns/Makefile
@@ -402,8 +404,8 @@ ver1=`grep "KERNEL_PATCHVER:="  target/linux/x86/Makefile | cut -d = -f 2` #判�
 export VER2="$(grep "KERNEL_PATCHVER:="  ./target/linux/x86/Makefile | cut -d = -f 2)"
 
 date1='Ipv4-Bypass-Vip-R'`TZ=UTC-8 date +%Y.%m.%d -d +"12"hour`
-# date1='Ipv4-Bypass-Vip-R2022.11.01'
-# sed -i 's/$(VERSION_DIST_SANITIZED)-$(IMG_PREFIX_VERNUM)$(IMG_PREFIX_VERCODE)$(IMG_PREFIX_EXTRA)/20221101-Ipv4-Bypass-Vip-5.10-/g' include/image.mk
+date1='Ipv4-Bypass-Vip-R2022.12.01'
+sed -i 's/$(VERSION_DIST_SANITIZED)-$(IMG_PREFIX_VERNUM)$(IMG_PREFIX_VERCODE)$(IMG_PREFIX_EXTRA)/20221201-Ipv4-Bypass-Vip-5.4-/g' include/image.mk
 if [ "$VER2" = "5.4" ]; then
     sed -i 's/$(VERSION_DIST_SANITIZED)-$(IMG_PREFIX_VERNUM)$(IMG_PREFIX_VERCODE)$(IMG_PREFIX_EXTRA)/$(shell TZ=UTC-8 date +%Y%m%d -d +12hour)-Ipv4-Bypass-Vip-5.4-/g' include/image.mk
 elif [ "$VER2" = "5.10" ]; then
