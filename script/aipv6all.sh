c88b07bb5d8c618bb749ddf09ee6e6631135f388
loso3000 <51484662+loso3000@users.noreply.github.com>
2022-12-14 00:25:09 +0800

    gk

--- script/aipv6all.sh
+++ script/aipv6all.sh
@@ -23,6 +23,8 @@ git clone https://github.com/loso3000/other ./package/other
 git clone https://github.com/sirpdboy/luci-app-lucky ./package/lucky
 git clone https://github.com/sirpdboy/luci-app-ddns-go ./package/ddns-go
 
+#管控
+sed -i 's/gk-jzgk/control-parentcontrol/g' ./package/other/up/luci-app-gk-jzgk/Makefile
 # nlbwmon
 sed -i 's/524288/16777216/g' feeds/packages/net/nlbwmon/files/nlbwmon.config
 
