212817b0d42804274cec6544ec9e6407e664bac9
loso3000 <51484662+loso3000@users.noreply.github.com>
2022-12-14 00:24:49 +0800

    gk

--- script/aipv6all-lean.sh
+++ script/aipv6all-lean.sh
@@ -26,6 +26,9 @@ rm -rf feeds/*/*/{luci-app-dockerman,luci-app-aria2,luci-app-beardropper,oaf,luc
 git clone https://github.com/loso3000/other ./package/other
 git clone https://github.com/sirpdboy/sirpdboy-package ./package/diy
 
+#管控
+sed -i 's/gk-jzgk/control-parentcontrol/g' ./package/other/up/luci-app-gk-jzgk/Makefile
+
 # git clone https://github.com/sirpdboy/luci-app-lucky ./package/lucky
 # git clone https://github.com/sirpdboy/luci-app-ddns-go ./package/ddns-go
 # nlbwmon
