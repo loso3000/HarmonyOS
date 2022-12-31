c8affc6a640bf9c3a4bfb45f99796aba23481112
loso3000 <51484662+loso3000@users.noreply.github.com>
2022-12-31 23:43:27 +0800

    l

--- script/aipv6all-lean.sh
+++ script/aipv6all-lean.sh
@@ -30,7 +30,8 @@ git clone https://github.com/sirpdboy/sirpdboy-package ./package/diy
 #管控
 sed -i 's/gk-jzgk/control-parentcontrol/g' ./package/other/up/luci-app-gk-jzgk/Makefile
 
-# git clone https://github.com/sirpdboy/luci-app-lucky ./package/lucky
+
+ git clone https://github.com/gdy666/luci-app-lucky ./package/luck ./package/lucky
 # git clone https://github.com/sirpdboy/luci-app-ddns-go ./package/ddns-go
 # nlbwmon
 sed -i 's/524288/16777216/g' feeds/packages/net/nlbwmon/files/nlbwmon.config
