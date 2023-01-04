0bd3479b37a1868a0f6227411017f7e5548a75c5
loso3000 <51484662+loso3000@users.noreply.github.com>
2023-01-04 14:25:51 +0800

    ccc

--- script/diy-clash-im.sh
+++ script/diy-clash-im.sh
@@ -2,7 +2,6 @@
 # rm -rf /package/lean/r8168/patches/020-5.18-support.patch
 mkdir -p files/etc/openclash/core
 # sed -i "s/PKG_HASH:=.*/PKG_HASH:=5de8c8e29aaa3fb9cc6b47bb27299f271354ebb72514e3accadc7d38b5bbaa72/g"  ./feeds/packages/utils/jq/Makefile
-
 open_clash_main_url=$(curl -sL https://api.github.com/repos/vernesong/OpenClash/releases/tags/Clash | grep /clash-linux-$1 | sed 's/.*url\": \"//g' | sed 's/\"//g')
 # offical_clash_main_url=$(curl -sL https://api.github.com/repos/Dreamacro/clash/releases/tags/v1.3.5 | grep /clash-linux-$1 | sed 's/.*url\": \"//g' | sed 's/\"//g')
 clash_tun_url=$(curl -sL https://api.github.com/repos/vernesong/OpenClash/releases/tags/TUN-Premium | grep /clash-linux-$1 | sed 's/.*url\": \"//g' | sed 's/\"//g')
@@ -12,7 +11,6 @@ wget -qO- $open_clash_main_url | tar xOvz > files/etc/openclash/core/clash
 # wget -qO- $offical_clash_main_url | gunzip -c > files/etc/openclash/core/clash
 wget -qO- $clash_tun_url | gunzip -c > files/etc/openclash/core/clash_tun
 wget -qO- $clash_game_url | tar xOvz > files/etc/openclash/core/clash_game
-
 chmod +x files/etc/openclash/core/clash*
 
 DEVICE_PLATFORM=$(cat .config | grep CONFIG_TARGET_ARCH_PACKAGES | awk -F '"' '{print $2}')
