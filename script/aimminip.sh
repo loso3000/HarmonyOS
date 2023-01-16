b3576e21b5920f4355ca84af7a0948a3a9c9bd14
loso3000 <51484662+loso3000@users.noreply.github.com>
2023-01-16 10:40:26 +0800

    n

--- script/aimminip.sh
+++ script/aimminip.sh
@@ -31,7 +31,7 @@ sed -i 's/gk-jzgk/control-parentcontrol/g' ./package/other/up/luci-app-gk-jzgk/M
 git clone https://github.com/sirpdboy/luci-app-autotimeset  ./package/luci-app-autotimeset
 
 # rm -rf ./feeds/luci/applications/netspeedtest
-svn export https://github.com/sirpdboy/netspeedtest  ./packages/netspeedtest
+# svn export https://github.com/sirpdboy/netspeedtest  ./packages/netspeedtest
 
 # nlbwmon
 sed -i 's/524288/16777216/g' feeds/packages/net/nlbwmon/files/nlbwmon.config
