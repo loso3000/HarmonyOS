72639edfb0875801e00e04a44c59977df25f3b96
loso3000 <51484662+loso3000@users.noreply.github.com>
2023-01-27 16:20:15 +0800

    5

--- script/asuper.sh
+++ script/asuper.sh
@@ -508,8 +508,8 @@ export VER2="$(grep "KERNEL_PATCHVER:="  ./target/linux/x86/Makefile | cut -d = 
 
 date1='Ipv6-Super-Vip-R'`TZ=UTC-8 date +%Y.%m.%d -d +"12"hour`
 
-# date1='Ipv6-Super-Vip-R2023.01.01'
-# sed -i 's/$(VERSION_DIST_SANITIZED)-$(IMG_PREFIX_VERNUM)$(IMG_PREFIX_VERCODE)$(IMG_PREFIX_EXTRA)/20230101-Ipv6-Super-Vip-5.4-/g' include/image.mk
+date1='Ipv6-Super-Vip-R2023.02.01'
+sed -i 's/$(VERSION_DIST_SANITIZED)-$(IMG_PREFIX_VERNUM)$(IMG_PREFIX_VERCODE)$(IMG_PREFIX_EXTRA)/20230201-Ipv6-Super-Vip-5.10-/g' include/image.mk
 if [ "$VER2" = "5.4" ]; then
     sed -i 's/$(VERSION_DIST_SANITIZED)-$(IMG_PREFIX_VERNUM)$(IMG_PREFIX_VERCODE)$(IMG_PREFIX_EXTRA)/$(shell TZ=UTC-8 date +%Y%m%d -d +12hour)-Ipv6-Super-Vip-5.4-/g' include/image.mk
 elif [ "$VER2" = "5.10" ]; then
