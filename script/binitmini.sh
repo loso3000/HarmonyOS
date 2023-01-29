2022c216998cf3ae56a34c109efddcb3c3dca601
loso3000 <51484662+loso3000@users.noreply.github.com>
2023-01-29 17:47:18 +0800

    21

--- script/binitmini.sh
+++ script/binitmini.sh
@@ -516,8 +516,8 @@ ver1=`grep "KERNEL_PATCHVER:="  target/linux/x86/Makefile | cut -d = -f 2` #åˆ¤æ
 export VER2="$(grep "KERNEL_PATCHVER:="  ./target/linux/x86/Makefile | cut -d = -f 2)"
 
 date1='Ipv6-Mini-Sirpdboy-R'`TZ=UTC-8 date +%Y.%m.%d -d +"12"hour`
-# date1='Ipv6-Mini-R2023.01.09'
-# sed -i 's/$(VERSION_DIST_SANITIZED)-$(IMG_PREFIX_VERNUM)$(IMG_PREFIX_VERCODE)$(IMG_PREFIX_EXTRA)/20230109-Ipv6-Mini-5.15-/g' include/image.mk
+date1='Ipv6-Mini-R2023.02.01'
+sed -i 's/$(VERSION_DIST_SANITIZED)-$(IMG_PREFIX_VERNUM)$(IMG_PREFIX_VERCODE)$(IMG_PREFIX_EXTRA)/20230201-Ipv6-Mini-6.1-/g' include/image.mk
 if [ "$VER2" = "5.4" ]; then
     sed -i 's/$(VERSION_DIST_SANITIZED)-$(IMG_PREFIX_VERNUM)$(IMG_PREFIX_VERCODE)$(IMG_PREFIX_EXTRA)/$(shell TZ=UTC-8 date +%Y%m%d -d +12hour)-Ipv6-Mini-5.4-/g' include/image.mk
 elif [ "$VER2" = "5.10" ]; then
