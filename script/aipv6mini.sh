aed43c8204278506709e16f20a71ed4ebbfbde20
loso3000 <51484662+loso3000@users.noreply.github.com>
2023-02-23 18:00:24 +0800

    x

--- script/aipv6mini.sh
+++ script/aipv6mini.sh
@@ -29,14 +29,15 @@ sed -i 's/gk-jzgk/control-parentcontrol/g' ./package/other/up/luci-app-gk-jzgk/M
 
 # netwizard
 sed -i 's/owizard/netwizard/g' ./package/other/up/luci-app-owizard/Makefile
-
 echo advancedplus
 mv -f  ./package/other/up/luci-app-pdadplus ./package/other/up/luci-app-advancedplus
 sed -i 's/pdadplus/advancedplus/g' ./package/other/up/luci-app-advancedplus/Makefile
+sed -i 's/pdadplus/advancedplus/g' ./package/other/up/luci-app-pdadplus/Makefile
 
 echo kucat
 mv -f  ./package/other/up/luci-app-xkucat ./package/other/up/luci-app-kucat
 sed -i 's/xkucat/kucat/g' ./package/other/up/luci-app-kucat/Makefile
+sed -i 's/xkucat/kucat/g' ./package/other/up/luci-app-xkucat/Makefile
 
 # svn export https://github.com/sirpdboy/sirpdboy-package/trunk/luci-app-autotimeset  ./package/luci-app-autotimeset
 
