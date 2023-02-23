5a3654eadc8b8fecb0a1e2348c8be48056b57e96
loso3000 <51484662+loso3000@users.noreply.github.com>
2023-02-23 12:27:19 +0800

    e

--- script/aipv6all-lean.sh
+++ script/aipv6all-lean.sh
@@ -32,10 +32,10 @@ sed -i 's/owizard/netwizard/g' ./package/other/up/luci-app-owizard/Makefile
 #advancedplus
 mv -f  ./package/other/up/luci-app-pdadplus./package/other/up/luci-app-advancedplus
 sed -i 's/pdadplus/advancedplus/g' ./package/other/up/luci-app-advancedplus/Makefile
-# kucat
+echo 'xkucat'
 mv -f  ./package/other/up/luci-app-xkucat ./package/other/up/luci-app-kucat
 sed -i 's/xkucat/kucat/g' ./package/other/up/luci-app-kucat/Makefile
-
+cp -r ./package/other/up/luci-app-kucat ./feeds/luci/applications/luci-app-kucat
 
 #sed -i 's/-D_GNU_SOURCE/-D_GNU_SOURCE -Wno-error=use-after-free/g' ./package/libs/elfutils/Makefile
 
