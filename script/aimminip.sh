c0c4aebd1f3c06e48e9a900c55a3795c851fa4ca
loso3000 <51484662+loso3000@users.noreply.github.com>
2023-02-23 13:34:57 +0800

    w

--- script/aimminip.sh
+++ script/aimminip.sh
@@ -32,10 +32,11 @@ sed -i 's/gk-jzgk/control-parentcontrol/g' ./package/other/up/luci-app-gk-jzgk/M
 # netwizard
 sed -i 's/owizard/netwizard/g' ./package/other/up/luci-app-owizard/Makefile
 
-#advancedplus
-mv -f  ./package/other/up/luci-app-pdadplus./package/other/up/luci-app-advancedplus
+echo advancedplus
+mv -f  ./package/other/up/luci-app-pdadplus ./package/other/up/luci-app-advancedplus
 sed -i 's/pdadplus/advancedplus/g' ./package/other/up/luci-app-advancedplus/Makefile
-# kucat
+
+echo kucat
 mv -f  ./package/other/up/luci-app-xkucat ./package/other/up/luci-app-kucat
 sed -i 's/xkucat/kucat/g' ./package/other/up/luci-app-kucat/Makefile
 
