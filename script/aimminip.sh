ea4875c18efe6e28c2cb4acd5b50e60f86c03a70
loso3000 <51484662+loso3000@users.noreply.github.com>
2023-02-23 17:59:31 +0800

    x

--- script/aimminip.sh
+++ script/aimminip.sh
@@ -35,10 +35,12 @@ sed -i 's/owizard/netwizard/g' ./package/other/up/luci-app-owizard/Makefile
 echo advancedplus
 mv -f  ./package/other/up/luci-app-pdadplus ./package/other/up/luci-app-advancedplus
 sed -i 's/pdadplus/advancedplus/g' ./package/other/up/luci-app-advancedplus/Makefile
+sed -i 's/pdadplus/advancedplus/g' ./package/other/up/luci-app-pdadplus/Makefile
 
 echo kucat
 mv -f  ./package/other/up/luci-app-xkucat ./package/other/up/luci-app-kucat
 sed -i 's/xkucat/kucat/g' ./package/other/up/luci-app-kucat/Makefile
+sed -i 's/xkucat/kucat/g' ./package/other/up/luci-app-xkucat/Makefile
 
 # autotimeset
 svn export https://github.com/sirpdboy/sirpdboy-package/trunk/luci-app-autotimeset  ./package/luci-app-autotimeset
