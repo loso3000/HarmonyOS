d7b6a8be5b0debd6e05ddebae4a6e7584e129522
loso3000 <51484662+loso3000@users.noreply.github.com>
2023-02-24 13:56:34 +0800

    b

--- script/aipv6mini.sh
+++ script/aipv6mini.sh
@@ -35,12 +35,15 @@ echo advancedplus
 svn export https://github.com/loso3000/mypk/trunk/up/luci-app-pdadplus ./package/lean/luci-app-advancedplus
 # rm -rf   ./package/other/up/luci-app-pdadplus
 sed -i 's/pdadplus/advancedplus/g' ./package/other/up/luci-app-pdadplus
+sed -i 's/pdadplus/advancedplus/g' ./package/lean/luci-app-advancedplus
 
 echo kucat
 # mv -f  ./package/other/up/luci-app-xkucat ./package/other/up/luci-app-kucat
 svn export https://github.com/loso3000/mypk/trunk/up/luci-theme-qcatku ./package/lean/luci-app-kucat
 # rm -rf   ./package/other/up/luci-app-xkucat
 sed -i 's/qcatku/kucat/g' ./package/other/up/luci-app-xkucat
+sed -i 's/qcatku/kucat/g' ./package/lean/luci-app-kucat
+
 
 # alist
 git clone https://github.com/sbwml/luci-app-alist package/alist
