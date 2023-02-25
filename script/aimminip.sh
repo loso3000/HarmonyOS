2fb43ac4cf61218ee442ddf868f7eba02f033559
loso3000 <51484662+loso3000@users.noreply.github.com>
2023-02-25 14:25:08 +0800

    a

--- script/aimminip.sh
+++ script/aimminip.sh
@@ -35,16 +35,16 @@ sed -i 's/gk-jzgk/control-parentcontrol/g' ./package/other/up/luci-app-gk-jzgk/M
 sed -i 's/owizard/netwizard/g' ./package/other/up/luci-app-owizard/Makefile
 
 echo advancedplus
-svn export https://github.com/loso3000/mypk/trunk/up/luci-app-pdadplus ./package/lean/luci-app-advancedplus
-# rm -rf   ./package/other/up/luci-app-pdadplus
-# sed -i 's/pdadplus/advancedplus/g' ./package/lean/luci-app-advancedplus
+#svn export https://github.com/loso3000/mypk/trunk/up/luci-app-pdadplus ./package/lean/luci-app-advancedplus
+svn export https://github.com/loso3000/mypk/trunk/up/luci-app-gkplus ./package/lean/luci-app-advancedplus
+sed -i 's/pdadplus/advancedplus/g' ./package/lean/luci-app-advancedplus
 
 echo kucat
 # mv -f  ./package/other/up/luci-app-xkucat ./package/other/up/luci-app-kucat
-# svn export https://github.com/loso3000/mypk/trunk/up/luci-theme-qcat ./package/lean/luci-app-kucat
+# svn export https://github.com/loso3000/mypk/trunk/up/luci-theme-qcat ./package/lean/luci-theme-kucat
 svn export https://github.com/loso3000/mypk/trunk/up/luci-theme-pdcat ./package/lean/luci-theme-kucat
 # rm -rf   ./package/other/up/luci-app-xkucat
-# sed -i 's/qcatku/kucat/g' ./package/lean/luci-app-kucat
+sed -i 's/qcatku/kucat/g' ./package/lean/luci-app-kucat
 
 # alist
 git clone https://github.com/sbwml/luci-app-alist package/alist
