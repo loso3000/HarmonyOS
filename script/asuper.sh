f68972f6f99aaa8e86acaebf76489a631da3499a
loso3000 <51484662+loso3000@users.noreply.github.com>
2023-02-27 13:52:08 +0800

    k

--- script/asuper.sh
+++ script/asuper.sh
@@ -31,7 +31,11 @@ git clone https://github.com/sirpdboy/luci-app-ddns-go  ./package/ddns-go
 sed -i 's/gk-jzgk/control-parentcontrol/g' ./package/other/up/luci-app-gk-jzgk/Makefile
 
 echo advancedplus
-svn export https://github.com/loso3000/mypk/trunk/up/luci-app-gkad ./package/lean/luci-app-advancedplus
+rm -rf  ./package/other/up/luci-app-advanced
+rm -rf  ./package/other/diy/luci-app-advanced
+rm -rf ./feeds/luci/applications/luci-app-advanced
+# svn export https://github.com/loso3000/mypk/trunk/up/luci-app-gkad ./package/lean/luci-app-advancedplus
+svn export https://github.com/loso3000/mypk/trunk/up/luci-app-kuplus ./package/lean/luci-app-advancedplus
 #svn export https://github.com/loso3000/mypk/trunk/up/luci-app-gkplus ./package/lean/luci-app-advancedplus
 sed -i 's/pdadplus/advancedplus/g' ./package/lean/luci-app-advancedplus
 
