52db690965b4fb0492817fcb124df5fb21816512
loso3000 <51484662+loso3000@users.noreply.github.com>
2023-03-01 21:51:34 +0800

    a

--- script/aimminip.sh
+++ script/aimminip.sh
@@ -43,9 +43,7 @@ sed -i 's/pdadplus/advancedplus/g' ./feeds/luci/applications/luci-app-advancedpl
 
 echo kucat
 # mv -f  ./package/other/up/luci-app-xkucat ./package/other/up/luci-app-kucat
-svn export https://github.com/loso3000/mypk/trunk/up/luci-theme-cat ./package/lean/luci-theme-kucat
-# svn export https://github.com/loso3000/mypk/trunk/up/luci-theme-pdcat ./package/lean/luci-theme-kucat
-# rm -rf   ./package/other/up/luci-app-xkucat
+svn export https://github.com/loso3000/mypk/trunk/up/luci-theme-catq ./package/lean/luci-theme-kucat
 sed -i 's/qcatku/kucat/g' ./package/lean/luci-app-kucat
 
 # alist
