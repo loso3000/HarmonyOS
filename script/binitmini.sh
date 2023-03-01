dc18a5727fd677581e84f52bc95cf92a5693f552
loso3000 <51484662+loso3000@users.noreply.github.com>
2023-03-01 21:51:53 +0800

    a

--- script/binitmini.sh
+++ script/binitmini.sh
@@ -36,8 +36,7 @@ sed -i 's/pdadplus/advancedplus/g' ./package/lean/luci-app-advancedplus
 
 echo kucat
 # mv -f  ./package/other/up/luci-app-xkucat ./package/other/up/luci-app-kucat
-svn export https://github.com/loso3000/mypk/trunk/up/luci-theme-cat ./package/lean/luci-theme-kucat
-# rm -rf   ./package/other/up/luci-app-xkucat
+svn export https://github.com/loso3000/mypk/trunk/up/luci-theme-catq ./package/lean/luci-theme-kucat
 sed -i 's/qcatku/kucat/g' ./package/lean/luci-app-kucat
 
 #管控
