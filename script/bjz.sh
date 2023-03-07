fcdb8bade965e70448d9573f70c2c6e641d8d3fc
loso3000 <51484662+loso3000@users.noreply.github.com>
2023-03-07 10:26:40 +0800

    x

--- script/bjz.sh
+++ script/bjz.sh
@@ -393,8 +393,12 @@ rm -rf ./feeds/packages/net/v2ray-core
 svn export https://github.com/fw876/helloworld/trunk/v2ray-core package/lean/v2ray-core
 svn export https://github.com/fw876/helloworld/trunk/hysteria package/lean/hysteria
 svn export https://github.com/fw876/helloworld/trunk/sagernet-core package/lean/sagernet-core
+
+# svn export https://github.com/fw876/helloworld/trunk/xray-core package/lean/xray-core
 rm -rf ./feeds/packages/net/xray-core
-svn export https://github.com/fw876/helloworld/trunk/xray-core package/lean/xray-core
+svn export https://github.com/loso3000/openwrt-passwall/trunk/xray-core  package/passwall/xray-core
+svn export https://github.com/loso3000/openwrt-passwall/trunk/xray-core  ./feeds/packages/net/xray-core
+
 svn export https://github.com/fw876/helloworld/trunk/v2ray-plugin package/lean/v2ray-plugin
 svn export https://github.com/fw876/helloworld/trunk/xray-plugin package/lean/xray-plugin
 svn export https://github.com/fw876/helloworld/trunk/shadowsocks-rust package/lean/shadowsocks-rust
