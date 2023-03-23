ffb613759f47b9317e427a1f0c0bd1ed980872d3
loso3000 <51484662+loso3000@users.noreply.github.com>
2023-03-23 13:09:22 +0800

    x

--- script/aipv6mini.sh
+++ script/aipv6mini.sh
@@ -62,11 +62,11 @@ sed -i "s/procd_add_jail transmission log/procd_add_jail_mount '$web_home'/g"  f
 
 # sed -i 's/-D_GNU_SOURCE/-D_GNU_SOURCE -Wno-error=use-after-free/g' ./package/libs/elfutils/Makefile
 
-# rm -rf ./feeds/luci/applications/netspeedtest
-# svn export https://github.com/sirpdboy/netspeedtest  ./packages/netspeedtest
-
 #  coremark
 sed -i '/echo/d' ./feeds/packages/utils/coremark/coremark
+
+# rm -rf ./feeds/luci/applications/netspeedtest
+# svn export https://github.com/sirpdboy/netspeedtest  ./packages/netspeedtest
 
 
 #  git clone https://github.com/gdy666/luci-app-lucky ./package/luck ./package/lucky
@@ -383,52 +383,54 @@ echo ' ShadowsocksR Plus+'
 # rm -rf  ./package/ssr/luci-app-ssr-plus
 # ShadowsocksR Plus+ 依赖
 rm -rf ./feeds/packages/net/kcptun
-svn export https://github.com/xiaorouji/openwrt-passwall/trunk/tcping package/new/tcping
-svn export https://github.com/xiaorouji/openwrt-passwall/trunk/brook package/new/brook
 
-svn export https://github.com/xiaorouji/openwrt-passwall/trunk/brook feeds/packages/net/brook
+svn export https://github.com/xiaorouji/openwrt-passwall/branches/packages/tcping package/new/tcping
+svn export https://github.com/xiaorouji/openwrt-passwall/branches/packages/brook package/new/brook
 
-svn export https://github.com/xiaorouji/openwrt-passwall/trunk/ssocks package/new/ssocks
-svn export https://github.com/xiaorouji/openwrt-passwall/trunk/microsocks package/new/microsocks
-svn export https://github.com/xiaorouji/openwrt-passwall/trunk/dns2socks package/new/dns2socks
-svn export https://github.com/xiaorouji/openwrt-passwall/trunk/ipt2socks package/new/ipt2socks
-svn export https://github.com/xiaorouji/openwrt-passwall/trunk/pdnsd-alt package/new/pdnsd
+svn export https://github.com/xiaorouji/openwrt-passwall/branches/packages/brook feeds/packages/net/brook
 
-svn export https://github.com/xiaorouji/openwrt-passwall/trunk/trojan-go package/new/trojan-go
-svn export https://github.com/xiaorouji/openwrt-passwall/trunk/trojan package/new/trojan
-svn export https://github.com/xiaorouji/openwrt-passwall/trunk/trojan-plus package/new/trojan-plus
+svn export https://github.com/xiaorouji/openwrt-passwall/branches/packages/ssocks package/new/ssocks
+svn export https://github.com/xiaorouji/openwrt-passwall/branches/packages/microsocks package/new/microsocks
+svn export https://github.com/xiaorouji/openwrt-passwall/branches/packages/dns2socks package/new/dns2socks
+svn export https://github.com/xiaorouji/openwrt-passwall/branches/packages/ipt2socks package/new/ipt2socks
+svn export https://github.com/xiaorouji/openwrt-passwall/branches/packages/pdnsd-alt package/new/pdnsd
+svn export https://github.com/xiaorouji/openwrt-passwall/branches/packages/sagernet-core package/new/sagernet-core
+svn export https://github.com/xiaorouji/openwrt-passwall/branches/packages/naiveproxy package/new/naiveproxy
+svn export https://github.com/xiaorouji/openwrt-passwall/branches/packages/trojan-go package/new/trojan-go
 
-# svn export https://github.com/QiuSimons/OpenWrt-Add/trunk/trojan-plus package/new/trojan-plus
+# svn export https://github.com/xiaorouji/openwrt-passwall/branches/packages/trojan package/new/trojan
+# svn export https://github.com/xiaorouji/openwrt-passwall/branches/packages/trojan-plus package/new/trojan-plus
+
+svn export https://github.com/QiuSimons/OpenWrt-Add/trunk/trojan-plus package/new/trojan-plus
+
 # svn export https://github.com/sirpdboy/openwrt-trojan-go/trunk/trojan-go package/new/trojan-go
-# svn export https://github.com/xiaorouji/openwrt-passwall/trunk/v2ray-geodata package/lean/v2ray-geodata  #用sbwml版本更多更好。
+# svn export https://github.com/xiaorouji/openwrt-passwall/branches/packages/v2ray-geodata package/lean/v2ray-geodata  #用sbwml版本更更好。
 # svn export https://github.com/QiuSimons/openwrt-mos/trunk/v2ray-geodata package/new/v2ray-geodata
 
-svn export https://github.com/fw876/helloworld/trunk/lua-neturl package/lean/lua-neturl
+svn export https://github.com/fw876/helloworld/trunk/lua-neturl package/new/lua-neturl
 rm -rf ./feeds/packages/net/shadowsocks-libev
-svn export https://github.com/coolsnowwolf/packages/trunk/net/shadowsocks-libev package/lean/shadowsocks-libev
-svn export https://github.com/fw876/helloworld/trunk/redsocks2 package/lean/redsocks2
-svn export https://github.com/coolsnowwolf/lede/trunk/package/lean/srelay package/lean/srelay
-svn export https://github.com/fw876/helloworld/trunk/trojan package/lean/trojan
-svn export https://github.com/fw876/helloworld/trunk/tcping package/lean/tcping
-svn export https://github.com/fw876/helloworld/trunk/dns2tcp package/lean/dns2tcp
-svn export https://github.com/fw876/helloworld/trunk/shadowsocksr-libev package/lean/shadowsocksr-libev
-svn export https://github.com/fw876/helloworld/trunk/simple-obfs package/lean/simple-obfs
-
-# svn export https://github.com/fw876/helloworld/trunk/naiveproxy package/lean/naiveproxy  #err
-svn export https://github.com/xiaorouji/openwrt-passwall/trunk/naiveproxy package/new/naiveproxy
+svn export https://github.com/coolsnowwolf/packages/trunk/net/shadowsocks-libev package/new/shadowsocks-libev
+svn export https://github.com/fw876/helloworld/trunk/redsocks2 package/new/redsocks2
+svn export https://github.com/coolsnowwolf/lede/trunk/package/lean/srelay package/new/srelay
+svn export https://github.com/fw876/helloworld/trunk/trojan package/new/trojan
+svn export https://github.com/fw876/helloworld/trunk/tcping package/new/tcping
+svn export https://github.com/fw876/helloworld/trunk/dns2tcp package/new/dns2tcp
+svn export https://github.com/fw876/helloworld/trunk/shadowsocksr-libev package/new/shadowsocksr-libev
+svn export https://github.com/fw876/helloworld/trunk/simple-obfs package/new/simple-obfs
 
 rm -rf ./feeds/packages/net/v2ray-core
 svn export https://github.com/fw876/helloworld/trunk/v2ray-core package/lean/v2ray-core
 svn export https://github.com/fw876/helloworld/trunk/hysteria package/lean/hysteria
-svn export https://github.com/fw876/helloworld/trunk/sagernet-core package/lean/sagernet-core
+
+
 # svn export https://github.com/fw876/helloworld/trunk/xray-core package/lean/xray-core
 rm -rf ./feeds/packages/net/xray-core
 svn export https://github.com/loso3000/openwrt-passwall/trunk/xray-core  package/passwall/xray-core
 svn export https://github.com/loso3000/openwrt-passwall/trunk/xray-core  ./feeds/packages/net/xray-core
 
-svn export https://github.com/fw876/helloworld/trunk/v2ray-plugin package/lean/v2ray-plugin
-svn export https://github.com/fw876/helloworld/trunk/xray-plugin package/lean/xray-plugin
-svn export https://github.com/fw876/helloworld/trunk/shadowsocks-rust package/lean/shadowsocks-rust
+svn export https://github.com/fw876/helloworld/trunk/v2ray-plugin package/new/v2ray-plugin
+svn export https://github.com/fw876/helloworld/trunk/xray-plugin package/new/xray-plugin
+svn export https://github.com/fw876/helloworld/trunk/shadowsocks-rust package/new/shadowsocks-rust
 rm -rf ./feeds/packages/net/kcptun
 svn export https://github.com/immortalwrt/packages/trunk/net/kcptun feeds/packages/net/kcptun
 ln -sf ../../../feeds/packages/net/kcptun ./package/feeds/packages/kcptun
@@ -451,7 +453,7 @@ sed -i '/Rust:/d' ./package/other/up/pass/luci-ssr-plusdns/Makefile
 # sed -i "s/bootstrap/chuqitopd/g" feeds/luci/modules/luci-base/root/etc/config/luci
 # sed -i 's/bootstrap/chuqitopd/g' feeds/luci/collections/luci/Makefile
 echo "修改默认主题"
-sed -i 's/+luci-theme-bootstrap/+luci-theme-opentopd/g' feeds/luci/collections/luci/Makefile
+sed -i 's/+luci-theme-bootstrap/+luci-theme-kucat/g' feeds/luci/collections/luci/Makefile
 # sed -i 's/+luci-theme-bootstrap/+luci-theme-opentopd/g' feeds/luci/collections/luci/Makefile
 # sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
 
@@ -474,7 +476,7 @@ rm -rf ./feeds/luci/applications/luci-theme-opentopd package/feeds/packages/luci
 
 # Remove some default packages
 # sed -i 's/luci-app-ddns//g;s/luci-app-upnp//g;s/luci-app-adbyby-plus//g;s/luci-app-vsftpd//g;s/luci-app-ssr-plus//g;s/luci-app-unblockmusic//g;s/luci-app-vlmcsd//g;s/luci-app-wol//g;s/luci-app-nlbwmon//g;s/luci-app-accesscontrol//g' include/target.mk
-sed -i 's/luci-app-adbyby-plus//g;s/luci-app-vsftpd//g;s/luci-app-ssr-plus//g;s/luci-app-unblockmusic//g;s/luci-app-vlmcsd//g;s/luci-app-wol//g;s/luci-app-nlbwmon//g;s/luci-app-accesscontrol//g' include/target.mk
+# sed -i 's/luci-app-adbyby-plus//g;s/luci-app-vsftpd//g;s/luci-app-ssr-plus//g;s/luci-app-unblockmusic//g;s/luci-app-vlmcsd//g;s/luci-app-wol//g;s/luci-app-nlbwmon//g;s/luci-app-accesscontrol//g' include/target.mk
 #Add x550
 git clone https://github.com/shenlijun/openwrt-x550-nbase-t package/openwrt-x550-nbase-t
 #修正nat回流 
