ea103c12ac7c0ab3a8017923fad2b40fbec14913
loso3000 <51484662+loso3000@users.noreply.github.com>
2023-04-11 01:36:28 +0800

    6

--- script/aipv6mini.sh
+++ script/aipv6mini.sh
@@ -482,8 +482,8 @@ cat ./package/other/patch/sysctl.conf > ./package/base-files/files/etc/sysctl.co
 cat ./package/other/patch/banner > ./package/base-files/files/etc/banner
 cat ./package/other/patch/profile > package/base-files/files/etc/profile
 cat ./package/other/patch/network.lua > ./feeds/luci/modules/luci-base/luasrc/model/network.lua
-# 6.1 80211 error
-cat ./package/other/patch/mac80211/intel.mk > ./package/kernel/mac80211/intel.mk
+# 6.1 80211 error 
+# cat ./package/other/patch/mac80211/intel.mk > ./package/kernel/mac80211/intel.mk
 cp -rf ./package/other/luci/*  ./feeds/luci/*
  
 
