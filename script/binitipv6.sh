28af39187dae90fe18e8186e7f9fcb67b86d0d1e
loso3000 <51484662+loso3000@users.noreply.github.com>
2023-04-24 17:56:22 +0800

    c

--- script/binitipv6.sh
+++ script/binitipv6.sh
@@ -179,12 +179,12 @@ rm -rf ./cfeeds/packages/net/smartdns
 svn export https://github.com/sirpdboy/sirpdboy-package/trunk/smartdns ./cfeeds/packages/net/smartdns
 
 # netdata 
-rm -rf ./feeds/luci/applications/luci-app-netdata package/feeds/packages/luci-app-netdata
-rm -rf ./feeds/packages/admin/netdata
-svn export https://github.com/sirpdboy/sirpdboy-package/trunk/luci-app-netdata ./feeds/luci/applications/luci-app-netdata
-svn export https://github.com/loso3000/mypk/trunk/up/netdata ./feeds/packages/admin/netdata
-ln -sf ../../../feeds/luci/applications/luci-app-netdata ./package/feeds/luci/luci-app-netdata
-rm -rf ./feeds/luci/applications/luci-app-netdata/root/etc/uci-defaults/
+rm -rf ./cfeeds/luci/applications/luci-app-netdata package/cfeeds/packages/luci-app-netdata
+rm -rf ./cfeeds/packages/admin/netdata
+svn export https://github.com/sirpdboy/sirpdboy-package/trunk/luci-app-netdata ./cfeeds/luci/applications/luci-app-netdata
+svn export https://github.com/loso3000/mypk/trunk/up/netdata ./cfeeds/packages/admin/netdata
+ln -sf ../../../cfeeds/luci/applications/luci-app-netdata ./package/feeds/luci/luci-app-netdata
+rm -rf ./fceeds/luci/applications/luci-app-netdata/root/etc/uci-defaults/
 
 
 rm -rf ./cfeeds/luci/applications/luci-app-arpbind
