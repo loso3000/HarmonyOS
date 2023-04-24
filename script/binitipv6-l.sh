a4474caf038bf5786884429b06ab70b0c94ca69e
loso3000 <51484662+loso3000@users.noreply.github.com>
2023-04-24 17:54:34 +0800

    n

--- script/binitipv6-l.sh
+++ script/binitipv6-l.sh
@@ -173,8 +173,12 @@ svn export https://github.com/sirpdboy/sirpdboy-package/trunk/smartdns ./feeds/p
 
 # netdata 
 rm -rf ./feeds/luci/applications/luci-app-netdata package/feeds/packages/luci-app-netdata
+rm -rf ./feeds/packages/admin/netdata
 svn export https://github.com/sirpdboy/sirpdboy-package/trunk/luci-app-netdata ./feeds/luci/applications/luci-app-netdata
+svn export https://github.com/loso3000/mypk/trunk/up/netdata ./feeds/packages/admin/netdata
 ln -sf ../../../feeds/luci/applications/luci-app-netdata ./package/feeds/luci/luci-app-netdata
+rm -rf ./feeds/luci/applications/luci-app-netdata/root/etc/uci-defaults/
+
 
 rm -rf ./feeds/luci/applications/luci-app-arpbind
 svn export https://github.com/loso3000/other/trunk/up/luci-app-arpbind ./feeds/luci/applications/luci-app-arpbind 
