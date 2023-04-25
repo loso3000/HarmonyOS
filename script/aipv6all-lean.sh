32aabc8aa213c34e9206f69d38aa6ff3086d0780
loso3000 <51484662+loso3000@users.noreply.github.com>
2023-04-25 20:50:47 +0800

    d

--- script/aipv6all-lean.sh
+++ script/aipv6all-lean.sh
@@ -124,8 +124,8 @@ git clone https://github.com/sbwml/v2ray-geodata package/geodata
 git clone https://github.com/sbwml/luci-app-alist.git package/luci-app-alist
 
 #dnsmasq
-rm -rf ./package/network/services/dnsmasq package/feeds/packages/dnsmasq
-svn export https://github.com/immortalwrt/immortalwrt/branches/openwrt-18.06-k5.4/package/network/services/dnsmasq ./package/network/services/dnsmasq
+#rm -rf ./package/network/services/dnsmasq package/feeds/packages/dnsmasq
+#svn export https://github.com/immortalwrt/immortalwrt/branches/openwrt-18.06-k5.4/package/network/services/dnsmasq ./package/network/services/dnsmasq
 
 #upnp
 #rm -rf ./feeds/packages/net/miniupnpd
