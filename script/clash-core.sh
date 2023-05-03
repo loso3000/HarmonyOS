c56680cc6c7509f53b3953fb50eff4bfd89352b9
loso3000 <51484662+loso3000@users.noreply.github.com>
2023-05-03 15:01:05 +0800

    z

--- script/clash-core.sh
+++ script/clash-core.sh
@@ -50,7 +50,8 @@ git clone https://github.com/robbyrussell/oh-my-zsh ./.oh-my-zsh
 git clone https://github.com/zsh-users/zsh-autosuggestions ./.oh-my-zsh/custom/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ./.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-completions ./.oh-my-zsh/custom/plugins/zsh-completions
-cp $GITHUB_WORKSPACE/file/zsh/.zshrc .
+cp ../../../file/zsh/.zshrc .
+cp ../../../file/zsh/zshrc .zshrc
 popd
 
 # speedtest
