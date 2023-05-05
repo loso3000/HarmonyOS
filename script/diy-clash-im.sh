6fd236acb477fccab97c7ef75e5adaf1d1ae8ccb
loso3000 <51484662+loso3000@users.noreply.github.com>
2023-05-05 15:15:26 +0800

    z

--- script/diy-clash-im.sh
+++ script/diy-clash-im.sh
@@ -53,10 +53,11 @@ git clone https://github.com/zsh-users/zsh-completions ./.oh-my-zsh/custom/plugi
 
 # Get .zshrc dotfile
 # cp $GITHUB_WORKSPACE/scripts/.zshrc .
+# curl -fsSL  https://raw.githubusercontent.com/loso3000/other/master/patch/z.zshrc > ./.zshrc
 curl -fsSL  https://raw.githubusercontent.com/loso3000/other/master/patch/z.zshrc > ./.zshrc
 popd
 
-# mv -f ./package/other/patch/z.zshrc ./files/root/.zshrc
+mv -f ./package/other/patch/z.zshrc ./files/root/.zshrc
 mv -f ./package/other/patch/profiles ./files/etc/profiles
 ls -a ./files/root
 echo "------------------------------------------------------"
