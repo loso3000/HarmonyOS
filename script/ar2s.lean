1a9118d63993a955b485dea785e4b17b4497d229
loso3000 <51484662+loso3000@users.noreply.github.com>
2023-06-10 20:02:18 +0800

    a

--- script/ar2s.lean
+++ script/ar2s.lean
@@ -318,7 +318,9 @@ CONFIG_PACKAGE_luci-app-ddns-go=y
 CONFIG_PACKAGE_luci-app-lucky=n
 CONFIG_PACKAGE_luci-app-partexp=y
 
-CONFIG_PACKAGE_luci-app-netspeedlimit=y
+CONFIG_PACKAGE_luci-app-control-speedlimit=n
+CONFIG_PACKAGE_luci-app-netspeedlimit=n
+CONFIG_PACKAGE_luci-app-eqosplus=y
 CONFIG_PACKAGE_luci-app-chatgpt=y
 CONFIG_PACKAGE_luci-app-chatgpt-web=y
 #docker
