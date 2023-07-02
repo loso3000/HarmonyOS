5c4bdc2d89a1aade9797bcb0a1374d0de90bf50f
loso3000 <51484662+loso3000@users.noreply.github.com>
2023-07-02 23:40:14 +0800

    p

--- script/aipv6mini.sh
+++ script/aipv6mini.sh
@@ -52,7 +52,9 @@ svn export https://github.com/loso3000/mypk/trunk/up/luci-app-autotimeset ./pack
 # rm -rf ./package/network/utils/iproute2/
 # svn export https://github.com/openwrt/openwrt/trunk/package/network/utils/iproute2 ./package/network/utils/iproute2
 
-mv -f  ./package/other/up/luci-app-eqosp ./package/other/up/luci-app-eqosplus
+mv -f  ./package/other/up/luci-app-eqosp ./package/other/up/luci-app-nqos
+mv -f  ./package/other/up/luci-app-eqospl ./package/other/up/luci-app-eqosplus
+rm -rf ./feeds/luci/applications/luci-app-p910nd
 rm -rf ./package/diy/luci-app-wrtbwmon
 rm -rf ./feeds/packages/net/wrtbwmon ./package/feeds/packages/wrtbwmon
 rm -rf ./feeds/luci/applications/luci-app-wrtbwmon ./package/feeds/packages/luci-app-wrtbwmon
