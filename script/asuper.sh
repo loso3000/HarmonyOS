4c6e6eddbb7254ab7732270efb0e0f22d49c2998
loso3000 <51484662+loso3000@users.noreply.github.com>
2023-07-20 02:44:02 +0800

    x

--- script/asuper.sh
+++ script/asuper.sh
@@ -427,6 +427,7 @@ sed -i 's,default n,default y,g' package/bypass/luci-app-bypass/Makefile
 sed -i 's,default n,default y,g' package/other/up/pass/luci-app-ssr-plus/Makefile
 sed -i 's,default n,default y,g' package/other/up/pass/luci-app-ssr-plusdns/Makefile
 
+svn export https://github.com/fw876/helloworld/trunk/tuic-client package/new/tuic-client
 rm -rf ./feeds/packages/net/xray-core
 svn export https://github.com/loso3000/openwrt-passwall/trunk/xray-core  package/passwall/xray-core
 svn export https://github.com/loso3000/openwrt-passwall/trunk/xray-core  ./feeds/packages/net/xray-core
