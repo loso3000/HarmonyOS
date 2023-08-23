e70a270b1290f0c85a2ae953b800466b612a163a
loso3000 <51484662+loso3000@users.noreply.github.com>
2023-08-24 02:13:20 +0800

    m

--- script/ar2s.sh
+++ script/ar2s.sh
@@ -127,19 +127,21 @@ mv -f  ./package/other/luci-app-mwan3 ./feeds/luci/applications/luci-app-mwan3
 # cp -f ./package/other/patch/mwan3  ./feeds/packages/net/mwan3/files/etc/config/mwan3
 # cat   ./package/other/patch/mwan3 > ./feeds/packages/net/mwan3/files/etc/config/mwan3
 
-# rm -rf ./feeds/packages/net/mosdns
-# rm -rf ./package/other/up/pass/mosdns
-# rm -rf ./feeds/packages/net/mosdns ./package/feeds/packages/mosdns
+
+rm -rf ./feeds/packages/net/mosdns
+rm -rf ./package/other/up/pass/mosdns
+rm -rf ./feeds/packages/net/mosdns ./package/feeds/packages/mosdns
 # svn export https://github.com/sbwml/luci-app-mosdns/branches/v4/mosdns feeds/packages/net/mosdns
 # svn export https://github.com/sbwml/luci-app-mosdns/branches/v4/mosdns package/new/mosdns
 # rm -rf package/feeds/packages/luci-app-mosdns ./feeds/luci/applications/luci-app-mosdns
-# rm -rf ./feeds/luci/applications/luci-app-mosdns
-# rm -rf feeds/packages/net/v2ray-geodata
+rm -rf ./feeds/luci/applications/luci-app-mosdns
+rm -rf feeds/packages/net/v2ray-geodata
 
 git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
-rm -rf package/mosdns/mosdns
-rm -rf package/mosdns/luci-app-mosdns
+# rm -rf package/mosdns/mosdns
+# rm -rf package/mosdns/luci-app-mosdns
+
 
 # 添加额外软件包alist
 git clone https://github.com/sbwml/luci-app-alist package/alist
