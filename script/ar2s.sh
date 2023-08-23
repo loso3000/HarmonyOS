be6df50f36ccbac25c1f3d067fad7afea016f1c1
loso3000 <51484662+loso3000@users.noreply.github.com>
2023-08-24 02:03:53 +0800

    a

--- script/ar2s.sh
+++ script/ar2s.sh
@@ -141,8 +141,9 @@ git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 rm -rf package/mosdns/mosdns
 rm -rf package/mosdns/luci-app-mosdns
 
-# alist
+# 添加额外软件包alist
 git clone https://github.com/sbwml/luci-app-alist package/alist
+sed -i 's/网络存储/存储/g' ./package/alist/luci-app-alist/po/zh-cn/alist.po
 rm -rf feeds/packages/lang/golang
 svn export https://github.com/sbwml/packages_lang_golang/branches/19.x feeds/packages/lang/golang
 
