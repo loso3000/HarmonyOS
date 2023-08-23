04dea3a2538f71dbb484545d93e8542469b0d48b
loso3000 <51484662+loso3000@users.noreply.github.com>
2023-08-23 10:30:09 +0800

    h

--- script/ar5s.sh
+++ script/ar5s.sh
@@ -610,13 +610,14 @@ sed -i 's,1512,1608,g' feeds/luci/applications/luci-app-cpufreq/root/etc/uci-def
 sed -i '/mt7662u_rom_patch.bin/a\\techo mt76-usb disable_usb_sg=1 > $\(1\)\/etc\/modules.d\/mt76-usb' package/kernel/mt76/Makefile
 
 # mt7921 mt7916
-rm -rf package/kernel/mac80211
-rm -rf package/kernel/rtl8821cu
-rm -rf package/network/services/hostapd
-svn export https://github.com/openwrt/openwrt/trunk/package/kernel/mac80211 package/kernel/mac80211
-svn export https://github.com/openwrt/openwrt/trunk/package/network/services/hostapd package/network/services/hostapd
-rm -rf package/kernel/mac80211/Makefile
-cp -f $GITHUB_WORKSPACE/Makefile package/kernel/mac80211/Makefile
+# rm -rf package/kernel/mac80211
+# rm -rf package/kernel/rtl8821cu
+# rm -rf package/network/services/hostapd
+# svn export https://github.com/openwrt/openwrt/trunk/package/kernel/mac80211 package/kernel/mac80211
+# svn export https://github.com/openwrt/openwrt/trunk/package/network/services/hostapd package/network/services/hostapd
+# rm -rf package/kernel/mac80211/Makefile
+# cp -f $GITHUB_WORKSPACE/Makefile package/kernel/mac80211/Makefile
+
 # panfrost gpu
 # rm ./target/linux/rockchip/modules.mk
 # rm ./package/kernel/linux/modules/video.mk
