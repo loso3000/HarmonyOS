72644b237b20a8c078c884b42a51bba8ea47f3ee
loso3000 <51484662+loso3000@users.noreply.github.com>
2023-08-25 12:22:44 +0800

    a

--- script/aimminip.sh
+++ script/aimminip.sh
@@ -93,6 +93,7 @@ rm -rf feeds/packages/net/v2ray-geodata
 
 # git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+git clone https://github.com/sbwml/v2ray-geodata feeds/packages/net/v2ray-geodata
 # rm -rf package/mosdns/mosdns
 # rm -rf package/mosdns/luci-app-mosdns
 
