ac84df5fc5d0a195467f9e572cf6812907e6b020
loso3000 <51484662+loso3000@users.noreply.github.com>
2023-08-26 14:36:26 +0800

    a

--- script/aipv6all-lean.sh
+++ script/aipv6all-lean.sh
@@ -139,10 +139,10 @@ rm -rf ./feeds/packages/net/mosdns ./package/feeds/packages/mosdns
 rm -rf ./feeds/luci/applications/luci-app-mosdns
 rm -rf feeds/packages/net/v2ray-geodata
 
-# git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
-# rm -rf package/mosdns/mosdns
-# rm -rf package/mosdns/luci-app-mosdns
+rm -rf package/mosdns/mosdns
+rm -rf package/mosdns/luci-app-mosdns
 
 # alist 
 git clone https://github.com/sbwml/luci-app-alist package/alist
