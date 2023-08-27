bdbe4dd31049439dd73c30ecbe5f3f8267ca91eb
loso3000 <51484662+loso3000@users.noreply.github.com>
2023-08-27 15:38:33 +0800

    m

--- script/asuper.sh
+++ script/asuper.sh
@@ -142,8 +142,8 @@ rm -rf feeds/packages/net/v2ray-geodata
 git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 git clone https://github.com/sbwml/v2ray-geodata feeds/packages/net/v2ray-geodata
-rm -rf package/mosdns/mosdns
-rm -rf package/mosdns/luci-app-mosdns
+# rm -rf package/mosdns/mosdns
+# rm -rf package/mosdns/luci-app-mosdns
 
 # alist 
 git clone https://github.com/sbwml/luci-app-alist package/alist
