64011c1241ccaccaa3a2230c67f60b51b34da7d0
loso3000 <51484662+loso3000@users.noreply.github.com>
2023-09-23 08:35:13 +0800

    w

--- script/aipv6all.lean
+++ script/aipv6all.lean
@@ -347,6 +347,7 @@ CONFIG_PACKAGE_luci-app-control-speedlimit=n
 CONFIG_PACKAGE_luci-app-netspeedlimit=n
 CONFIG_PACKAGE_luci-app-eqosplus=y
 CONFIG_PACKAGE_luci-app-chatgpt=y
+CONFIG_PACKAGE_luci-app-chatgpt-web=y
 CONFIG_PACKAGE_luci-app-arpbind=y
 
 
