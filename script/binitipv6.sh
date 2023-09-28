daf4facac7a48931c7669eba3f8e165cdbd03b96
loso3000 <51484662+loso3000@users.noreply.github.com>
2023-09-28 11:14:34 +0800

    ssr

--- script/binitipv6.sh
+++ script/binitipv6.sh
@@ -139,6 +139,10 @@ git clone https://github.com/sbwml/packages_lang_golang -b 20.x feeds/packages/l
 #sed -i "/filter_aaaa='1'/d" ./cfeeds/luci/applications/luci-app-mosdns/root/etc/init.d/mosdns
 #sed -i "/filter_aaaa='1'/d" ./package/new/luci-app-mosdns/root/etc/init.d/mosdns
 
+#package/libs/openssl 
+rm -rf package/libs/openssl
+svn export https://github.com/immortalwrt/immortalwrt/branches/openwrt-18.06-k5.4/package/libs/openssl ./package/libs/openssl
+
 #dnsmasq
 #rm -rf ./package/network/services/dnsmasq package/cfeeds/packages/dnsmasq
 #svn export https://github.com/immortalwrt/immortalwrt/branches/openwrt-18.06-k5.4/package/network/services/dnsmasq ./package/network/services/dnsmasq
