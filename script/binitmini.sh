f19eb76b27c6fbffa1ead57c3d992e2ddbb05e30
loso3000 <51484662+loso3000@users.noreply.github.com>
2023-09-29 08:43:43 +0800

    b

--- script/binitmini.sh
+++ script/binitmini.sh
@@ -147,8 +147,8 @@ rm -rf feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 20.x feeds/packages/lang/golang
 
 #package/libs/openssl 
-rm -rf package/libs/openssl
-svn export https://github.com/immortalwrt/immortalwrt/branches/openwrt-18.06-k5.4/package/libs/openssl ./package/libs/openssl
+#rm -rf package/libs/openssl
+#svn export https://github.com/immortalwrt/immortalwrt/branches/openwrt-18.06-k5.4/package/libs/openssl ./package/libs/openssl
 
 #dnsmasq
 #rm -rf ./package/network/services/dnsmasq package/feeds/packages/dnsmasq
