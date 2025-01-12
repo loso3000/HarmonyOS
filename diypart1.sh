c4b9fce75b652ed6a3fbfb6cbf17f6182772af0b
loso3000 sir <51484662+loso3000@users.noreply.github.com>
2025-01-12 22:45:16 +0800

    d

--- diypart1.sh
+++ diypart1.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 cp -Rf ../lede/.github/tmp/* .  || true
+ls  ../lede/.github/tmp/
+[ -f ./diyv ] || cp -Rf ./.github/tmp/* . 
+chmod +x diyv
+./diyv
+ls 
 [ -f ./diy/diy ] || cp -Rf ./.github/tmp/* . 
 chmod +x diy/diy
 ./diy/diy
