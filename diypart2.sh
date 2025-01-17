c1ed787745c500e41105b27b8f1ef41b43944483
loso3000 sir <51484662+loso3000@users.noreply.github.com>
2025-01-17 21:13:08 +0800

    d

--- diypart2.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-ls -a ../
-cp -Rf ../lede/.github/tmp/* .  || true
-ls -a ../lede/.github/tmp/*
-[ -f ./diy2.sh ] || cp -Rf ./.github/tmp/* . 
-ls -a ./.github/tmp
-chmod +x diy2.sh
-./diy2.sh
-ls -a
